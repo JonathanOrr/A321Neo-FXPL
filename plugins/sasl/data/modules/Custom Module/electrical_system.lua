e0a1fefc4e007a71b8931b511cdb51275fd1fa31
Serg Podtynnyi <serg@podtynnyi.com>
2020-02-23 15:17:10 +0300

    Update components

---
+++ plugins/sasl/data/modules/Custom Module/electrical_system.lua
@@ -0,0 +1,4 @@
+local ext_pwr_on = createGlobalPropertyi("a321neo/electrical/ext_pwr_on", 0)
+local apu_gen_on = createGlobalPropertyi("a321neo/electrical/apu_gen_on", 0)
+local gen1_on = createGlobalPropertyi("a321neo/electrical/gen1_on", 0)
+local gen2_on = createGlobalPropertyi("a321neo/electrical/gen2_on", 0)
---
+++ plugins/sasl/data/modules/Custom Module/main_inst_panel.lua
@@ -0,0 +1,4 @@
+components = {
+  PFD { position= {345,1623,192,192} },
+  ND  { position= {563,1625,192,192} }
+ }
