d076b695ce0fb964a6c4b0e66914d59e28dc8a49
Serg Podtynnyi <serg@podtynnyi.com>
2020-02-24 04:28:06 +0300

    Add helper for bitwise operations for buttons

--- plugins/sasl/data/modules/main.lua
+++ plugins/sasl/data/modules/main.lua
@@ -1,11 +1,11 @@
-local bit = require("bit")
+include("helpers.lua")
 
 sasl.options.setAircraftPanelRendering(true)
 sasl.options.setInteractivity(true)
 
-size = { 2048, 2048 }
+size = { 4096, 2048 }
 
-panelWidth3d = 2048
+panelWidth3d = 4096
 panelHeight3d = 2048
 
 
