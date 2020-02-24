6cd4089c57717703da2ef5e35b7545f306fcc737
Serg Podtynnyi <serg@podtynnyi.com>
2020-02-24 16:35:59 +0300

    Update helpers and electrics

--- plugins/sasl/data/modules/Custom Module/elec_panel.lua
+++ plugins/sasl/data/modules/Custom Module/elec_panel.lua
@@ -1,8 +1,9 @@
 -- READ
-local ext_pwr_on = globalProperty("a321neo/electrical/ext_pwr_on", 0)
+local ext_pwr_on = globalProperty("a321neo/electrical/ext_pwr_on")
 
 -- WRITE
 -- BITS: 1 bit for bottom part of the button, 2 bit is the upper part of the bottom
+-- local ext_pwr_button = globalProperty("a321neo/electrical/ext_pwr_button")
 local ext_pwr_button = createGlobalPropertyi("a321neo/electrical/ext_pwr_button", 0, false, true, true)
 
 -- CMD
@@ -12,15 +13,15 @@ local command_ext_pwr_button_toggle = createCommand("a321neo/electrical/ext_pwr_
 function on_command_ext_pwr_button_toggle (phase)
   if phase == SASL_COMMAND_BEGIN
   then
-    -- toggle the button ON bit
+    -- flip the button ON bit
     -- setting the view dataref
-    set(ext_pwr_button, bit_toggle(get(ext_pwr_button), 1))
+    set(ext_pwr_button, flipBitValue(get(ext_pwr_button), 1))
   end
 end
 
 
 function update_ext_pwr_button()
-  set(ext_pwr_button, bit_set_to_value(get(ext_pwr_button), 2, get(ext_pwr_on)))
+  set(ext_pwr_button, setBitValue(get(ext_pwr_button), 2, get(ext_pwr_on)))
 end
 
 function update()
--- plugins/sasl/data/modules/Custom Module/electrical_system.lua
+++ plugins/sasl/data/modules/Custom Module/electrical_system.lua
@@ -1,5 +1,58 @@
+
+local ext_pwr_button = globalProperty("a321neo/electrical/ext_pwr_button")
+
 local ext_pwr_on = createGlobalPropertyi("a321neo/electrical/ext_pwr_on", 0, false, true, false)
 local apu_gen_on = createGlobalPropertyi("a321neo/electrical/apu_gen_on", 0, false, true, true)
 local gen1_on = createGlobalPropertyi("a321neo/electrical/gen1_on", 0, false, true, true)
 local gen2_on = createGlobalPropertyi("a321neo/electrical/gen2_on", 0, false, true, true)
 local emer_gen_on = createGlobalPropertyi("a321neo/electrical/emer_gen_on", 0, false, true, true)
+
+local ac_bus1_on = createGlobalPropertyi("a321neo/electrical/ac_bus1_on", 0, false, true, true)
+local ac_bus2_on = createGlobalPropertyi("a321neo/electrical/ac_bus2_on", 0, false, true, true)
+local ac_ess_bus_on = createGlobalPropertyi("a321neo/electrical/ac_ess_bus_on", 0, false, true, true)
+
+local dc_bus1_on = createGlobalPropertyi("a321neo/electrical/dc_bus1_on", 0, false, true, true)
+local dc_bus2_on = createGlobalPropertyi("a321neo/electrical/dc_bus2_on", 0, false, true, true)
+local dc_bus_ent = createGlobalPropertyi("a321neo/electrical/dc_bus_ent_on", 0, false, true, true)
+local dc_bat_bus_on = createGlobalPropertyi("a321neo/electrical/dc_bat_bus_on", 0, false, true, true)
+local dc_ess_bus_on = createGlobalPropertyi("a321neo/electrical/dc_ess_bus_on", 0, false, true, true)
+
+local bat1_on = createGlobalPropertyi("a321neo/electrical/bat1_on", 0, false, true, true)
+local bat2_on = createGlobalPropertyi("a321neo/electrical/bat2_on", 0, false, true, true)
+
+
+function update()
+
+  if datarefIsOn(ext_pwr_on) and datarefIsOnBit(ext_pwr_button, 1)
+  then
+    datarefSetOn(ac_bus1_on)
+    datarefSetOn(ac_bus2_on)
+  end
+  --
+
+  if datarefIsOn(ac_bus1_on)
+  then
+    datarefSetOn(ac_ess_bus_on)
+    datarefSetOn(dc_bus1_on)
+  end
+
+
+  if datarefIsOn(dc_bus1_on)
+  then
+    datarefSetOn(dc_bat_bus_on)
+  end
+
+
+  if datarefIsOn(dc_bat_bus_on)
+  then
+    datarefSetOn(dc_ess_bus_on)
+  end
+
+
+  if datarefIsOn(emer_gen_on)
+  then
+    datarefSetOn(dc_ess_bus_on)
+    datarefSetOn(acc_ess_bus_on)
+  end
+
+end
