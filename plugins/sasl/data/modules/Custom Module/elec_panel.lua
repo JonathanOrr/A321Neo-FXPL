46d42f377887bb70be95f1c574308272fce99e83
Serg Podtynnyi <serg@podtynnyi.com>
2020-02-25 00:47:34 +0300

    Up[date electric systems

--- plugins/sasl/data/modules/Custom Module/elec_panel.lua
+++ plugins/sasl/data/modules/Custom Module/elec_panel.lua
@@ -1,31 +1,48 @@
 -- READ
 local ext_pwr_on = globalProperty("a321neo/electrical/ext_pwr_on")
+local dc_bat_bus_on = globalPropertyi("a321neo/electrical/dc_bat_bus_on")
+
 
 -- WRITE
 -- BITS: 1 bit for bottom part of the button, 2 bit is the upper part of the bottom
--- local ext_pwr_button = globalProperty("a321neo/electrical/ext_pwr_button")
-local ext_pwr_button = createGlobalPropertyi("a321neo/electrical/ext_pwr_button", 0, false, true, true)
+local ext_pwr_button = globalProperty("a321neo/electrical/ext_pwr_button")
+local bat1_button = globalProperty("a321neo/electrical/bat1_button")
+local bat2_button = globalProperty("a321neo/electrical/bat2_button")
+local bat1_on = globalPropertyi("a321neo/electrical/bat1_on")
+local bat2_on = globalPropertyi("a321neo/electrical/bat2_on")
+
 
 -- CMD
-local command_ext_pwr_button_toggle = createCommand("a321neo/electrical/ext_pwr_button_toggle", "Toggle EXT PWR")
+local command_ext_pwr_button_push = createCommand("a321neo/electrical/ext_pwr_button_push", "Push EXT PWR")
+local command_bat1_button_push = createCommand("a321neo/electrical/bat1_button_push", "Push BAT1")
+local command_bat2_button_push = createCommand("a321neo/electrical/bat2_button_push", "Push BAT2")
 
 
-function on_command_ext_pwr_button_toggle (phase)
-  if phase == SASL_COMMAND_BEGIN
-  then
-    -- flip the button ON bit
-    -- setting the view dataref
-    set(ext_pwr_button, flipBitValue(get(ext_pwr_button), 1))
+function on_command_ext_pwr_button_push (phase)
+  if phase == SASL_COMMAND_BEGIN then set(ext_pwr_button, flipBitValue(get(ext_pwr_button), 1)) end
+end
+
+function on_command_bat1_button_push (phase)
+  if phase == SASL_COMMAND_BEGIN then
+    datarefFlip(bat1_on)
+    set(bat1_button, setBitValue(get(bat1_button), 3, get(bat1_on)))
+  end
+end
+
+function on_command_bat2_button_push (phase)
+  if phase == SASL_COMMAND_BEGIN then
+    datarefFlip(bat2_on)
+    set(bat2_button, setBitValue(get(bat1_button), 3, get(bat2_on)))
   end
 end
 
 
-function update_ext_pwr_button()
-  set(ext_pwr_button, setBitValue(get(ext_pwr_button), 2, get(ext_pwr_on)))
-end
-
 function update()
-  update_ext_pwr_button()
+  set(ext_pwr_button, setBitValue(get(ext_pwr_button), 2, get(ext_pwr_on)))
+  set(bat1_button, setBitValue(get(bat1_button), 1, get(dc_bat_bus_on)))
+  set(bat2_button, setBitValue(get(bat2_button), 1, get(dc_bat_bus_on)))
 end
 
-sasl.registerCommandHandler(command_ext_pwr_button_toggle, 0, on_command_ext_pwr_button_toggle)
+sasl.registerCommandHandler(command_ext_pwr_button_push, 0, on_command_ext_pwr_button_push)
+sasl.registerCommandHandler(command_bat1_button_push, 0, on_command_bat1_button_push)
+sasl.registerCommandHandler(command_bat2_button_push, 0, on_command_bat2_button_push)
--- plugins/sasl/data/modules/Custom Module/electrical_system.lua
+++ plugins/sasl/data/modules/Custom Module/electrical_system.lua
@@ -1,5 +1,8 @@
 
-local ext_pwr_button = globalProperty("a321neo/electrical/ext_pwr_button")
+local ext_pwr_button = createGlobalPropertyi("a321neo/electrical/ext_pwr_button", 0, false, true, true)
+
+local bat1_button = createGlobalPropertyi("a321neo/electrical/bat1_button", 0, false, true, true)
+local bat2_button = createGlobalPropertyi("a321neo/electrical/bat2_button", 0, false, true, true)
 
 local ext_pwr_on = createGlobalPropertyi("a321neo/electrical/ext_pwr_on", 0, false, true, false)
 local apu_gen_on = createGlobalPropertyi("a321neo/electrical/apu_gen_on", 0, false, true, true)
@@ -11,6 +14,12 @@ local ac_bus1_on = createGlobalPropertyi("a321neo/electrical/ac_bus1_on", 0, fal
 local ac_bus2_on = createGlobalPropertyi("a321neo/electrical/ac_bus2_on", 0, false, true, true)
 local ac_ess_bus_on = createGlobalPropertyi("a321neo/electrical/ac_ess_bus_on", 0, false, true, true)
 
+-- trs not used yet
+local tr1_on = createGlobalPropertyi("a321neo/electrical/tr1_on", 0, false, true, true)
+local tr2_on = createGlobalPropertyi("a321neo/electrical/tr2_on", 0, false, true, true)
+local tr_ess_on = createGlobalPropertyi("a321neo/electrical/tr_ess_on", 0, false, true, true)
+local tr_ent_on = createGlobalPropertyi("a321neo/electrical/tr_ent_on", 0, false, true, true)
+
 local dc_bus1_on = createGlobalPropertyi("a321neo/electrical/dc_bus1_on", 0, false, true, true)
 local dc_bus2_on = createGlobalPropertyi("a321neo/electrical/dc_bus2_on", 0, false, true, true)
 local dc_bus_ent = createGlobalPropertyi("a321neo/electrical/dc_bus_ent_on", 0, false, true, true)
@@ -20,15 +29,41 @@ local dc_ess_bus_on = createGlobalPropertyi("a321neo/electrical/dc_ess_bus_on", 
 local bat1_on = createGlobalPropertyi("a321neo/electrical/bat1_on", 0, false, true, true)
 local bat2_on = createGlobalPropertyi("a321neo/electrical/bat2_on", 0, false, true, true)
 
+-- MISC
+local indicated_airspeed = globalProperty("sim/flightmodel/position/indicated_airspeed")
+local wheel_on_ground = globalProperty("sim/flightmodel2/gear/on_ground[0]")
+
+
+function exp_pwr_on_and_pb_on()
+  return (datarefIsOn(ext_pwr_on) and datarefIsOnBit(ext_pwr_button, 1))
+end
+
+--test
+local ac1_fault = false
+local ac2_fault = false
+
 
 function update()
 
-  if datarefIsOn(ext_pwr_on) and datarefIsOnBit(ext_pwr_button, 1)
+  -- Turn off ac_bus1_on and ac_bus2_on if no power
+  -- if exp_pwr_on_and_pb_on() == false and datarefIsOff(gen1_on) and datarefIsOff(gen2_on) and datarefIsOff(apu_gen_on)
+  -- then
+  datarefSetOff(ac_bus1_on)
+  datarefSetOff(ac_bus2_on)
+  datarefSetOff(ac_ess_bus_on)
+  datarefSetOff(dc_bus1_on)
+  datarefSetOff(dc_bus2_on)
+  datarefSetOff(dc_bus_ent)
+  datarefSetOff(dc_bat_bus_on)
+  datarefSetOff(dc_ess_bus_on)
+  -- end
+
+  -- all gens are interchangable
+  if exp_pwr_on_and_pb_on() or datarefIsOn(gen1_on) or  datarefIsOn(gen2_on) or datarefIsOn(apu_gen_on)
   then
-    datarefSetOn(ac_bus1_on)
-    datarefSetOn(ac_bus2_on)
+    if ac1_fault == false then datarefSetOn(ac_bus1_on) end
+    if ac2_fault == false then datarefSetOn(ac_bus2_on) end
   end
-  --
 
   if datarefIsOn(ac_bus1_on)
   then
@@ -36,23 +71,74 @@ function update()
     datarefSetOn(dc_bus1_on)
   end
 
+  if datarefIsOn(ac_bus2_on)
+  then
+    datarefSetOn(dc_bus2_on)
+    -- power dc_bus_ent if enough power
+    if (datarefIsOn(gen1_on) and datarefIsOn(gen2_on)) or exp_pwr_on_and_pb_on() or datarefIsOn(apu_gen_on) then datarefSetOn(dc_bus_ent) end
+  end
+
+  -- AC ESS FEED Auto Switching
+  if datarefIsOn(ac_bus2_on) and datarefIsOff(ac_bus1_on)
+  then
+    datarefSetOn(ac_ess_bus_on)
+  end
+
+  if datarefIsOn(ac_ess_bus_on) and datarefIsOff(ac_bus1_on)
+  then
+    datarefSetOn(dc_ess_bus_on)
+  end
 
   if datarefIsOn(dc_bus1_on)
   then
     datarefSetOn(dc_bat_bus_on)
-  end
-
-
-  if datarefIsOn(dc_bat_bus_on)
-  then
     datarefSetOn(dc_ess_bus_on)
   end
 
+  -- TODO: add delay
+  -- DC BUS 2 supplies DC BUS 1 and DC BAT BUS automatically after 5s.
+  if datarefIsOn(dc_bus2_on) and datarefIsOff(dc_bus1_on)
+  then
+    datarefSetOn(dc_bus1_on)
+    datarefSetOn(dc_bat_bus_on)
+  end
 
+
+  if datarefIsOn(bat1_on) and (datarefIsOff(dc_bus1_on) and datarefIsOff(dc_bus2_on))
+  then
+      datarefSetOn(ac_ess_bus_on)
+      datarefSetOn(dc_ess_bus_on)
+      datarefSetOff(dc_bat_bus_on)
+      if get(indicated_airspeed) < 100.0 then datarefSetOn(dc_bat_bus_on) end
+      if get(indicated_airspeed) < 50.0 then datarefSetOff(ac_ess_bus_on) end
+  end
+
+  if datarefIsOn(bat2_on) and (datarefIsOff(dc_bus1_on) and datarefIsOff(dc_bus2_on))
+  then
+    datarefSetOn(ac_ess_bus_on)
+    datarefSetOn(dc_ess_bus_on)
+    datarefSetOff(dc_bat_bus_on)
+    if get(indicated_airspeed) < 100.0 then datarefSetOn(dc_bat_bus_on) end
+    if get(indicated_airspeed) < 50.0 then datarefSetOff(ac_ess_bus_on) end
+  end
+
+  -- if datarefIsOn(dc_bat_bus_on)
+  -- then
+  -- end
+
+  -- handle if ac_bus1 and ac_bus2 lost in flight using RAT
+  if datarefIsOff(ac_bus1_on) and datarefIsOff(ac_bus2_on) and (get(indicated_airspeed) > 100.0 or datarefIsOff(wheel_on_ground) )
+  then
+    datarefSetOn(emer_gen_on)
+  else
+    datarefSetOff(emer_gen_on)
+  end
+
+  -- emer_gen_on powers only ess buss
   if datarefIsOn(emer_gen_on)
   then
+    datarefSetOn(ac_ess_bus_on)
     datarefSetOn(dc_ess_bus_on)
-    datarefSetOn(acc_ess_bus_on)
   end
 
 end
