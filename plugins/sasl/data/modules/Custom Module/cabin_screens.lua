083701a628956f229983e265c6a5915705183c8e
JonathanOrr <61325694+JonathanOrr@users.noreply.github.com>
2020-04-23 21:19:44 +1000

    cabin screen info page update

--- plugins/sasl/data/modules/Custom Module/cabin_screens.lua
+++ plugins/sasl/data/modules/Custom Module/cabin_screens.lua
@@ -15,7 +15,10 @@ end
 
 function update()
     set(cabin_screen_page, Math_clamp(get(cabin_screen_page), 0, 2))
-    
+
+    set(Distance_traveled_km, get(Distance_traveled_m) / 10)
+    set(Groudn_speed_kmh, get(Ground_speed_ms) * 3.6)
+
     if get(Engine_1_master_switch) == 0 or get(Engine_2_master_switch) == 0 then
         set(cabin_screen_page, 0)
     elseif get(Engine_1_master_switch) == 1 and get(Engine_2_master_switch) == 1 then
--- plugins/sasl/data/modules/Custom Module/global_datarefs.lua
+++ plugins/sasl/data/modules/Custom Module/global_datarefs.lua
@@ -1,7 +1,11 @@
 --global dataref for the A32NX project
+Distance_traveled_km = createGlobalPropertyf("a321neo/dynamics/distance_traveled_km", 0, false, true, false)
+Groudn_speed_kmh = createGlobalPropertyf("a321neo/dynamics/groundspeed_kmh", 0, false, true, false)
 Engine_1_master_switch = createGlobalPropertyi("a321neo/engine/master_1", 0, false, true, false)
 Engine_2_master_switch = createGlobalPropertyi("a321neo/engine/master_2", 0, false, true, false)
 
 --global dataref variable from the Sim
+Distance_traveled_m = globalProperty("sim/flightmodel/controls/dist")
+Ground_speed_ms = globalProperty("sim/flightmodel/position/groundspeed")
 Engine_1_avail = globalProperty("sim/flightmodel/engine/ENGN_running[0]")
 Engine_2_avail = globalProperty("sim/flightmodel/engine/ENGN_running[1]")
