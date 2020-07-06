f171d563f91e079f710f564ec4ba5888e611e6fe
JonathanOrr <61325694+JonathanOrr@users.noreply.github.com>
2020-07-06 23:17:37 +1000

    update wheels logic

--- plugins/sasl/data/modules/Custom Module/ECAM.lua
+++ plugins/sasl/data/modules/Custom Module/ECAM.lua
@@ -13,37 +13,37 @@ local apu_needle_state = createGlobalPropertyi("a321neo/cockpit/apu/apu_needle_s
 local B612MONO_regular = sasl.gl.loadFont("fonts/B612Mono-Regular.ttf")
 
 --colors
-local left_l_brake_temp_color = {1.0, 1.0, 1.0}
-local left_r_brake_temp_color = {1.0, 1.0, 1.0}
-local right_l_brake_temp_color = {1.0, 1.0, 1.0}
-local right_r_brake_temp_color = {1.0, 1.0, 1.0}
-local mcdu_white = {1.0, 1.0, 1.0}
-local mcdu_blue = {0.004, 1.0, 1.0}
-local mcdu_green = {0.004, 1, 0.004}
-local mcdu_orange = {0.843, 0.49, 0}
+local left_brake_temp_color = {1.0, 1.0, 1.0}
+local right_brake_temp_color = {1.0, 1.0, 1.0}
+local left_tire_psi_color = {1.0, 1.0, 1.0}
+local right_tire_psi_color = {1.0, 1.0, 1.0}
+local ECAM_WHITE = {1.0, 1.0, 1.0}
+local ECAM_BLUE = {0.004, 1.0, 1.0}
+local ECAM_GREEN = {0.004, 1, 0.004}
+local ECAM_ORANGE = {0.843, 0.49, 0.0}
 
 --custom fucntions
 local function draw_ecam_lower_section()
     --left section
-    sasl.gl.drawText(B612MONO_regular, size[1]/2-215, size[2]/2-373, math.floor(get(TAT)), 30, false, false, TEXT_ALIGN_RIGHT, mcdu_blue)
-    sasl.gl.drawText(B612MONO_regular, size[1]/2-215, size[2]/2-409, math.floor(get(OTA)), 30, false, false, TEXT_ALIGN_RIGHT, mcdu_blue)
+    sasl.gl.drawText(B612MONO_regular, size[1]/2-215, size[2]/2-373, math.floor(get(TAT)), 30, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
+    sasl.gl.drawText(B612MONO_regular, size[1]/2-215, size[2]/2-409, math.floor(get(OTA)), 30, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
 
     --center section
     --adding a 0 to the front of the time when single digit
     if get(ZULU_hours) < 10 then
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-25, size[2]/2-408, "0" .. get(ZULU_hours), 30, false, false, TEXT_ALIGN_RIGHT, mcdu_blue)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-25, size[2]/2-408, "0" .. get(ZULU_hours), 30, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
     else
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-25, size[2]/2-408, get(ZULU_hours), 30, false, false, TEXT_ALIGN_RIGHT, mcdu_blue)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-25, size[2]/2-408, get(ZULU_hours), 30, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
     end
 
     if get(ZULU_mins) < 10 then
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+25, size[2]/2-408, "0" .. get(ZULU_mins), 30, false, false, TEXT_ALIGN_LEFT, mcdu_blue)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+25, size[2]/2-408, "0" .. get(ZULU_mins), 30, false, false, TEXT_ALIGN_LEFT, ECAM_GREEN)
     else
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+25, size[2]/2-408, get(ZULU_mins), 30, false, false, TEXT_ALIGN_LEFT, mcdu_blue)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+25, size[2]/2-408, get(ZULU_mins), 30, false, false, TEXT_ALIGN_LEFT, ECAM_GREEN)
     end
 
     --right section
-    sasl.gl.drawText(B612MONO_regular, size[1]/2+375, size[2]/2-374, math.floor(get(Gross_weight)), 30, false, false, TEXT_ALIGN_RIGHT, mcdu_blue)
+    sasl.gl.drawText(B612MONO_regular, size[1]/2+375, size[2]/2-374, math.floor(get(Gross_weight)), 30, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
 end
 
 function update()
@@ -53,29 +53,30 @@ function update()
         set(apu_needle_state, 1)
     end
 
-    if get(Left_l_brakes_hot) == 1 then
-        left_l_brake_temp_color = mcdu_orange
-    else
-        left_l_brake_temp_color = mcdu_white
-    end
+    --wheels indications--
+    if get(Left_brakes_temp) > 400 then
+		left_brake_temp_color = ECAM_ORANGE
+	else
+		left_brake_temp_color = ECAM_WHITE
+	end
 
-    if get(Left_r_brakes_hot) == 1 then
-        left_r_brake_temp_color = mcdu_orange
-    else
-        left_r_brake_temp_color = mcdu_white
-    end
+	if get(Right_brakes_temp) > 400 then
+		right_brake_temp_color = ECAM_ORANGE
+	else
+		right_brake_temp_color = ECAM_WHITE
+	end
 
-    if get(Right_l_brakes_hot) == 1 then
-        right_l_brake_temp_color = mcdu_orange
-    else
-        right_l_brake_temp_color = mcdu_white
-    end
+	if get(Left_tire_psi) > 280 then
+		left_tire_psi_color = ECAM_ORANGE
+	else
+		left_tire_psi_color = ECAM_WHITE
+	end
 
-    if get(Right_r_brakes_hot) == 1 then
-        right_r_brake_temp_color = mcdu_orange
-    else
-        right_r_brake_temp_color = mcdu_white
-    end
+	if get(Right_tire_psi) > 280 then
+		right_tire_psi_color = ECAM_ORANGE
+	else
+		right_tire_psi_color = ECAM_WHITE
+	end
 end
 
 --drawing the ECAM
@@ -95,63 +96,70 @@ function draw()
     elseif get(Ecam_current_page) == 7 then --apu
         --apu gen section--
         if get(Apu_gen_state) == 2 then
-            sasl.gl.drawText(B612MONO_regular, size[1]/2-235, size[2]/2+257, math.floor(get(Apu_gen_load)), 23, false, false, TEXT_ALIGN_RIGHT, mcdu_green)
-            sasl.gl.drawText(B612MONO_regular, size[1]/2-235, size[2]/2+224, math.floor(get(Apu_gen_volts)), 23, false, false, TEXT_ALIGN_RIGHT, mcdu_green)
-            sasl.gl.drawText(B612MONO_regular, size[1]/2-235, size[2]/2+192, math.floor(get(Apu_gen_hz)), 23, false, false, TEXT_ALIGN_RIGHT, mcdu_green)
+            sasl.gl.drawText(B612MONO_regular, size[1]/2-235, size[2]/2+257, math.floor(get(Apu_gen_load)), 23, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
+            sasl.gl.drawText(B612MONO_regular, size[1]/2-235, size[2]/2+224, math.floor(get(Apu_gen_volts)), 23, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
+            sasl.gl.drawText(B612MONO_regular, size[1]/2-235, size[2]/2+192, math.floor(get(Apu_gen_hz)), 23, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
         end
         --apu bleed--
         if get(Apu_bleed_state) > 0 then
-            sasl.gl.drawText(B612MONO_regular, size[1]/2+270, size[2]/2+186, math.floor(get(Apu_bleed_psi)), 23, false, false, TEXT_ALIGN_RIGHT, mcdu_green)
+            sasl.gl.drawText(B612MONO_regular, size[1]/2+270, size[2]/2+186, math.floor(get(Apu_bleed_psi)), 23, false, false, TEXT_ALIGN_RIGHT, ECAM_GREEN)
         end
         --needles--
         if get(apu_needle_state) == 1 then
-            sasl.gl.drawText(B612MONO_regular, size[1]/2-180, size[2]/2-60, math.floor(get(Apu_N1)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-            sasl.gl.drawText(B612MONO_regular, size[1]/2-180, size[2]/2-260, math.floor(get(APU_EGT)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
+            sasl.gl.drawText(B612MONO_regular, size[1]/2-180, size[2]/2-60, math.floor(get(Apu_N1)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+            sasl.gl.drawText(B612MONO_regular, size[1]/2-180, size[2]/2-260, math.floor(get(APU_EGT)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
         end
     elseif get(Ecam_current_page) == 8 then --cond
         --cabin--
         --actual temperature
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-212, size[2]/2+210, math.floor(get(Cockpit_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-13, size[2]/2+210, math.floor(get(Front_cab_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+172, size[2]/2+210, math.floor(get(Aft_cab_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-212, size[2]/2+210, math.floor(get(Cockpit_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-13, size[2]/2+210, math.floor(get(Front_cab_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+172, size[2]/2+210, math.floor(get(Aft_cab_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
         --requested temperatures
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-212, size[2]/2+170, math.floor(get(Cockpit_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-13, size[2]/2+170, math.floor(get(Front_cab_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+172, size[2]/2+170, math.floor(get(Aft_cab_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-212, size[2]/2+170, math.floor(get(Cockpit_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-13, size[2]/2+170, math.floor(get(Front_cab_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+172, size[2]/2+170, math.floor(get(Aft_cab_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
 
         --cargo--
         --actual temperature
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+168, size[2]/2-59, math.floor(get(Aft_cargo_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+168, size[2]/2-59, math.floor(get(Aft_cargo_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
         --requested temperatures
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+168, size[2]/2-92, math.floor(get(Aft_cargo_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+168, size[2]/2-92, math.floor(get(Aft_cargo_temp_req)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
     elseif get(Ecam_current_page) == 9 then --door
 
     elseif get(Ecam_current_page) == 10 then --wheel
         --brakes temps--
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-360, size[2]/2-75, math.floor(get(Left_l_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-200, size[2]/2-75, math.floor(get(Left_r_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+200, size[2]/2-75, math.floor(get(Right_l_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+360, size[2]/2-75, math.floor(get(Right_r_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, mcdu_green)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-360, size[2]/2-75, math.floor(get(Left_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-200, size[2]/2-75, math.floor(get(Left_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+200, size[2]/2-75, math.floor(get(Right_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+360, size[2]/2-75, math.floor(get(Right_brakes_temp)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        --tire press
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-360, size[2]/2-165, math.floor(get(Left_tire_psi)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-200, size[2]/2-165, math.floor(get(Left_tire_psi)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+200, size[2]/2-165, math.floor(get(Right_tire_psi)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+360, size[2]/2-165, math.floor(get(Right_tire_psi)), 30, false, false, TEXT_ALIGN_CENTER, ECAM_GREEN)
         --brakes indications
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-280, size[2]/2-75, "°C", 26, false, false, TEXT_ALIGN_CENTER, mcdu_blue)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-280, size[2]/2-120, "REL", 26, false, false, TEXT_ALIGN_CENTER, mcdu_white)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+280, size[2]/2-75, "°C", 26, false, false, TEXT_ALIGN_CENTER, mcdu_blue)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+280, size[2]/2-120, "REL", 26, false, false, TEXT_ALIGN_CENTER, mcdu_white)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-360, size[2]/2-120, "1", 26, false, false, TEXT_ALIGN_CENTER, mcdu_white)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2-200, size[2]/2-120, "2", 26, false, false, TEXT_ALIGN_CENTER, mcdu_white)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+200, size[2]/2-120, "3", 26, false, false, TEXT_ALIGN_CENTER, mcdu_white)
-        sasl.gl.drawText(B612MONO_regular, size[1]/2+360, size[2]/2-120, "4", 26, false, false, TEXT_ALIGN_CENTER, mcdu_white)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-280, size[2]/2-75, "°C", 26, false, false, TEXT_ALIGN_CENTER, ECAM_BLUE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-280, size[2]/2-120, "REL", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-280, size[2]/2-165, "PSI", 26, false, false, TEXT_ALIGN_CENTER, ECAM_BLUE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+280, size[2]/2-75, "°C", 26, false, false, TEXT_ALIGN_CENTER, ECAM_BLUE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+280, size[2]/2-120, "REL", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+280, size[2]/2-165, "PSI", 26, false, false, TEXT_ALIGN_CENTER, ECAM_BLUE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-360, size[2]/2-120, "1", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2-200, size[2]/2-120, "2", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+200, size[2]/2-120, "3", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+        sasl.gl.drawText(B612MONO_regular, size[1]/2+360, size[2]/2-120, "4", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
 
         --upper arcs
-        sasl.gl.drawArc(size[1]/2 - 360, size[2]/2 - 110, 76, 80, 60, 60, left_l_brake_temp_color)
-        sasl.gl.drawArc(size[1]/2 - 200, size[2]/2 - 110, 76, 80, 60, 60, left_r_brake_temp_color)
-        sasl.gl.drawArc(size[1]/2 + 200, size[2]/2 - 110, 76, 80, 60, 60, right_l_brake_temp_color)
-        sasl.gl.drawArc(size[1]/2 + 360, size[2]/2 - 110, 76, 80, 60, 60, right_r_brake_temp_color)
+        sasl.gl.drawArc(size[1]/2 - 360, size[2]/2 - 110, 76, 80, 60, 60, left_brake_temp_color)
+        sasl.gl.drawArc(size[1]/2 - 200, size[2]/2 - 110, 76, 80, 60, 60, left_brake_temp_color)
+        sasl.gl.drawArc(size[1]/2 + 200, size[2]/2 - 110, 76, 80, 60, 60, right_brake_temp_color)
+        sasl.gl.drawArc(size[1]/2 + 360, size[2]/2 - 110, 76, 80, 60, 60, right_brake_temp_color)
         --lower arcs
-        sasl.gl.drawArc(size[1]/2 - 360, size[2]/2 - 110, 76, 80, 240, 60, mcdu_white)
-        sasl.gl.drawArc(size[1]/2 - 200, size[2]/2 - 110, 76, 80, 240, 60, mcdu_white)
-        sasl.gl.drawArc(size[1]/2 + 200, size[2]/2 - 110, 76, 80, 240, 60, mcdu_white)
-        sasl.gl.drawArc(size[1]/2 + 360, size[2]/2 - 110, 76, 80, 240, 60, mcdu_white)
+        sasl.gl.drawArc(size[1]/2 - 360, size[2]/2 - 110, 76, 80, 240, 60, left_tire_psi_color)
+        sasl.gl.drawArc(size[1]/2 - 200, size[2]/2 - 110, 76, 80, 240, 60, left_tire_psi_color)
+        sasl.gl.drawArc(size[1]/2 + 200, size[2]/2 - 110, 76, 80, 240, 60, right_tire_psi_color)
+        sasl.gl.drawArc(size[1]/2 + 360, size[2]/2 - 110, 76, 80, 240, 60, right_tire_psi_color)
 
     elseif get(Ecam_current_page) == 11 then --f/ctl
 
--- plugins/sasl/data/modules/Custom Module/wheel.lua
+++ plugins/sasl/data/modules/Custom Module/wheel.lua
@@ -1,4 +1,8 @@
 --sim dataref
+local left_brakes_temp_no_delay = 10
+local right_brakes_temp_no_delay = 10
+local left_tire_psi_no_delay = 210
+local right_tire_psi_no_delay = 210
 
 --a32nx dataref
 local groundspeed_kts = createGlobalPropertyf("a321neo/dynamics/groundspeed_kts", 0, false, true, false) --ground speed in kts
@@ -9,34 +13,43 @@ function update()
 
 	if get(Aft_wheel_on_ground) == 1 then
 		if get(Actual_brake_ratio) >  0 then
-			set(Left_l_brakes_temp, get(Left_l_brakes_temp) + (get(Actual_brake_ratio) * (0.05 * get(groundspeed_kts)) ^ 1.975) * get(DELTA_TIME))
-			set(Left_r_brakes_temp, get(Left_r_brakes_temp) + (get(Actual_brake_ratio) * (0.05 * get(groundspeed_kts)) ^ 1.975) * get(DELTA_TIME))
-			set(Right_l_brakes_temp, get(Right_l_brakes_temp) + (get(Actual_brake_ratio) * (0.05 * get(groundspeed_kts)) ^ 1.975) * get(DELTA_TIME))
-			set(Right_r_brakes_temp, get(Right_r_brakes_temp) + (get(Actual_brake_ratio) * (0.05 * get(groundspeed_kts)) ^ 1.975) * get(DELTA_TIME))
+			left_brakes_temp_no_delay = left_brakes_temp_no_delay + (get(Actual_brake_ratio) * ((0.05 * get(groundspeed_kts)) ^ 1.975) * get(DELTA_TIME))
+			right_brakes_temp_no_delay = right_brakes_temp_no_delay + (get(Actual_brake_ratio) * ((0.05 * get(groundspeed_kts)) ^ 1.975) * get(DELTA_TIME))
+		end
+
+		if get(Brakes_fan) == 1 then
+			--fan cooled
+			left_brakes_temp_no_delay = Set_anim_value(left_brakes_temp_no_delay, 10, -100, 1000, 0.00125)
+			right_brakes_temp_no_delay = Set_anim_value(right_brakes_temp_no_delay, 10, -100, 1000, 0.00125)
+		else
+			--natural cool down
+			left_brakes_temp_no_delay = Set_anim_value(left_brakes_temp_no_delay, 10, -100, 1000, 0.00075)
+			right_brakes_temp_no_delay = Set_anim_value(right_brakes_temp_no_delay, 10, -100, 1000, 0.00075)
+		end
+	else
+		if get(Brakes_fan) == 1 then
+			--fan cooled
+			left_brakes_temp_no_delay = Set_anim_value(left_brakes_temp_no_delay, 10, -100, 1000, Math_clamp(((39/160000) * get(IAS)) + 0.00125, 0.00125, 0.05))
+			right_brakes_temp_no_delay = Set_anim_value(right_brakes_temp_no_delay, 10, -100, 1000, Math_clamp(((39/160000) * get(IAS)) + 0.00125, 0.00125, 0.05))
+		else
+			--natural cool down
+			left_brakes_temp_no_delay = Set_anim_value(left_brakes_temp_no_delay, 10, -100, 1000, Math_clamp(((197/800000) * get(IAS)) + 0.00075, 0.00125, 0.05))
+			right_brakes_temp_no_delay = Set_anim_value(right_brakes_temp_no_delay, 10, -100, 1000, Math_clamp(((197/800000) * get(IAS)) + 0.00075, 0.00125, 0.05))
 		end
 	end
 
-	if get(Left_l_brakes_temp) > 400 then
-		set(Left_l_brakes_hot, 1)
-	else
-		set(Left_l_brakes_hot, 0)
-	end
+	left_tire_psi_no_delay = 5/39 * (left_brakes_temp_no_delay - 10) + 210
+	right_tire_psi_no_delay = 5/39 * (right_brakes_temp_no_delay - 10) + 210
 
-	if get(Left_r_brakes_temp) > 400 then
-		set(Left_r_brakes_hot, 1)
-	else
-		set(Left_r_brakes_hot, 0)
-	end
+	--set(Left_brakes_temp, left_brakes_temp_no_delay)
+	--set(Right_brakes_temp, right_brakes_temp_no_delay)
 
-	if get(Right_l_brakes_temp) > 400 then
-		set(Right_l_brakes_hot, 1)
-	else
-		set(Right_l_brakes_hot, 0)
-	end
+	--set(Left_tire_psi,  left_tire_psi_no_delay)
+	--set(Right_tire_psi, left_tire_psi_no_delay)
 
-	if get(Right_r_brakes_temp) > 400 then
-		set(Right_r_brakes_hot, 1)
-	else
-		set(Right_r_brakes_hot, 0)
-	end
+	set(Left_brakes_temp, Set_anim_value(get(Left_brakes_temp), left_brakes_temp_no_delay, -100, 1000, 0.5))
+	set(Right_brakes_temp, Set_anim_value(get(Left_brakes_temp), right_brakes_temp_no_delay, -100, 1000, 0.5))
+
+	set(Left_tire_psi,  Set_anim_value(get(Left_tire_psi), left_tire_psi_no_delay, -100, 1000, 0.5))
+	set(Right_tire_psi, Set_anim_value(get(Left_tire_psi), left_tire_psi_no_delay, -100, 1000, 0.5))
 end
