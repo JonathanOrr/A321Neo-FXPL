61cecf901d9bfc76a3b312609c2707a41c17c7be
Chai <chch22@patana.ac.th>
2020-07-07 22:50:57 +0700

    add glyph spacing and page handling

--- plugins/sasl/data/modules/Custom Module/MCDU.lua
+++ plugins/sasl/data/modules/Custom Module/MCDU.lua
@@ -35,7 +35,12 @@ local MCDU_DISP_TEXT_SIZE =
     ["s"] = MCDU_DRAW_TEXT_SIZE.s,
     ["l"] = MCDU_DRAW_TEXT_SIZE.l,
 }
-local MCDU_DISP_ALIGN =
+local MCDU_DISP_TEXT_SPACING =
+{
+    ["s"] = 1.667,
+    ["l"] = 1.0,
+}
+local MCDU_DISP_TEXT_ALIGN =
 {
     ["L"] = TEXT_ALIGN_LEFT,
     ["C"] = TEXT_ALIGN_CENTER,
@@ -44,6 +49,10 @@ local MCDU_DISP_ALIGN =
 
 --fonts
 local B612MONO_regular = sasl.gl.loadFont("fonts/B612Mono-Regular.ttf")
+
+-- alphanumeric & decimal FMC entry keys
+local MCDU_ENTRY_KEYS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."}
+local MCDU_ENTRY_PAGES = {"DIR", "PROG", "PERF", "INIT", "DATA", "F-PLN", "RAD NAV", "FUEL PRED", "SEC F-PLN", "ATC COMM", "MCDU MENU", "AIRP"}
 
 
 --[[
@@ -88,9 +97,8 @@ local mcdu_clr_key = createCommand("a321neo/cockpit/mcdu/clr", "MCDU CLR Key")
 local mcdu_page_up = sasl.createCommand("a321neo/cockpit/mcdu/page_up", "MCDU page up Key")
 local mcdu_page_dn = sasl.createCommand("a321neo/cockpit/mcdu/page_dn", "MCDU page down Key")
 --mcdu entry inputs
---alphanumeric and decimal
-local MCDU_ENTRY_KEYS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."}
 
+--alphanumeric and decimal
 local mcdu_inp_key = {}
 for i,key in ipairs(MCDU_ENTRY_KEYS) do
 	-- create the command
@@ -105,51 +113,18 @@ for i,key in ipairs(MCDU_ENTRY_KEYS) do
 	end)
 end
 
---[[
---
---
---      FMC DATA INITIALIZATION
---
---
---]]
-
--- init all rows to format as color "white"
-local mcdu_dat = {}
-local mcdu_dat_title = {txt = "TITLE", col = "white"}
-for i,size in ipairs(MCDU_DIV_SIZE) do
-	mcdu_dat[size] = {}
-	for j,align in ipairs(MCDU_DIV_ALIGN) do
-		mcdu_dat[size][align] = {}
-		for k,row in ipairs(MCDU_DIV_ROW) do
-			-- print(size .. ", " .. align .. ", " .. row)
-			-- init in here
-			--mcdu_dat[size][align][row] = {txt = size .. "" .. align .. " " .. row, col = "white"}
-			mcdu_dat[size][align][row] = {txt = "dot", col = "white"}
+for i,page in ipairs(MCDU_ENTRY_PAGES) do
+	-- create the command
+	mcdu_inp_page[page] = createCommand("a321neo/cockpit/mcdu/" .. page, "MCDU Character " .. page .. " page")
+	-- register the command
+	sasl.registerCommandHandler(mcdu_inp_page[page], 0, function (phase)
+		if phase == SASL_COMMAND_BEGIN then
+            mcdu_clear_all()
+            set(mcdu_page, i * 100)
 		end
-	end
+	end)
 end
 
---sasl variables
---mcdu lines(max munber of character is 22)
-local mcdu_title_L = "TITLE"
-local mcdu_title_M = "TITLE"
-local mcdu_title_R = "TITLE"
---MCDU left section
---entry line
-local mcdu_entry = ""
-local mcdu_messages = {}
-
---mcdu status
-local mcdu_message_active = 0
-local mcdu_init_status = 0
-
---define custom functionalities
-local function send_mcdu_message(message, status)
-    table.insert(mcdu_messages, message)
-    mcdu_message_active = status
-end
-
---registering command handlers last as putting in variables coming after the handler will crash the plugin
 --sim command handlers
 
 --a321neo command handlers
@@ -192,8 +167,6 @@ sasl.registerCommandHandler(mcdu_clr_key, 0, function (phase)
     end
 end)
 
-
-
 sasl.registerCommandHandler(mcdu_page_up, 0, function (phase)
     if phase == SASL_COMMAND_BEGIN then
         mcdu_fpln_page = mcdu_fpln_page + 1
@@ -206,11 +179,116 @@ sasl.registerCommandHandler(mcdu_page_dn, 0, function (phase)
     end
 end)
 
+--[[
+--
+--
+--      MCDU DATA INITIALIZATION
+--
+--
+--]]
+
+-- init all rows to format as color "white"
+local mcdu_dat = {}
+for i,size in ipairs(MCDU_DIV_SIZE) do
+	mcdu_dat[size] = {}
+	for j,align in ipairs(MCDU_DIV_ALIGN) do
+		mcdu_dat[size][align] = {}
+	end
+end
+
+local mcdu_dat_title_L = {txt = "TITLE", col = "white"}
+local mcdu_dat_title_C = {txt = "TITLE", col = "white"}
+local mcdu_dat_title_R = {txt = "TITLE", col = "white"}
+
+--sasl variables
+--MCDU left section
+--entry line
+local mcdu_entry = ""
+local mcdu_messages = {}
+
+--mcdu status
+local mcdu_message_active = 0
+local mcdu_init_status = 0
+
+--[[
+--
+--
+--      MCDU PAGE SIMULATION
+--
+--      loosely based on
+--      http://www.a320dp.com/A320_DP/nav-flight-management/sys-14.0.0.html
+--      
+--      WARNING - the website has an outdated MCDU, consult ToLiss for actual data
+--      14.7.5 would be (0705 - 200) so 0505 so 505
+--
+--      0 - nothing
+--      505 - A/C Status
+--
+--
+--]]
+
+local mcdu_sim_page = {}
+
+--define custom functionalities
+local function send_mcdu_message(message, status)
+    table.insert(mcdu_messages, message)
+    mcdu_message_active = status
+end
+
+local function mcdu_clearall()
+    mcdu_dat_title_L = {txt = "", col = "white"}
+    mcdu_dat_title_C = {txt = "", col = "white"}
+    mcdu_dat_title_R = {txt = "", col = "white"}
+    for i,size in ipairs(MCDU_DIV_SIZE) do
+        for j,align in ipairs(MCDU_DIV_ALIGN) do
+            for k,row in ipairs(MCDU_DIV_ROW) do
+                --mcdu_dat[size][align][row] = {txt = size .. "" .. align .. " " .. row, col = "white"}
+                mcdu_dat[size][align][row] = {txt = "", col = "white"}
+            end
+        end
+    end
+end
+
+--registering command handlers last as putting in variables coming after the handler will crash the plugin
 
 function update()
     if get(mcdu_page) == 0 then
+        mcdu_clearall()
+        set(mcdu_page, 505)
+    end
+    mcdu_sim_page[get(mcdu_page)]("render")
+end
+
+-- 505 A/C Status
+mcdu_sim_page[505] =
+function (phase)
+    if phase == "render" then
+        mcdu_dat_title_C = "A321 NEO"
+
+        mcdu_dat["s", "L", 1].txt = "eng"
+
+        if get(Engine_option) == 0 then
+            mcdu_dat["l", "L", 1] = {txt = "cfm-leap-1a", col = "blue"}
+        else
+            mcdu_dat["l", "L", 1] = {txt = "pw-1130g-jm", col = "blue"}
+        end
+        
+        mcdu_dat["s", "L", 2].txt = "active nav data base"
+        mcdu_dat["s", "L", 3].txt = "second nav data base"
+
+        mcdu_dat["s", "L", 5].txt = "chg code"
+        mcdu_dat["s", "L", 6].txt = "idle/perf"
+       
     end
 end
+
+--[[
+--
+--
+--      MCDU DRAWING
+--
+--
+--]]
 
 --drawing the MCDU display
 function draw()
@@ -239,17 +317,20 @@ function draw()
 
                     -- text size 
                     disp_text_size = MCDU_DISP_TEXT_SIZE[draw_size]
+                    sasl.gl.setFontGlyphSpacingFactor(B612MONO_regular, MCDU_DISP_TEXT_SPACING[draw_size])
+
                     -- text alignment
-                    disp_align = MCDU_DISP_ALIGN[draw_align]
+                    disp_text_align = MCDU_DISP_TEXT_ALIGN[draw_align]
+                    
 
                     -- text data
                     --print(draw_size .. " " .. draw_align .. " " .. draw_row)
                     dat = mcdu_dat[draw_size][draw_align][draw_row]
-                    disp_text = dat.txt
+                    disp_text = dat.txt:upper()
                     disp_color = MCDU_DISP_COLOR[dat.col]
 
                     -- now draw it!
-                    sasl.gl.drawText(B612MONO_regular, disp_x, disp_y, disp_text, disp_text_size, false, false, disp_align, disp_color)
+                    sasl.gl.drawText(B612MONO_regular, disp_x, disp_y, disp_text, disp_text_size, false, false, disp_text_align, disp_color)
                 end
             end
         end
