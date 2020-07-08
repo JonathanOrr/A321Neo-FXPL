5dde714642dad3cd0ebae0dcc40ff8ac57d1ff5d
JonathanOrr <61325694+JonathanOrr@users.noreply.github.com>
2020-07-08 21:35:29 +1000

    update PACKs logic

--- plugins/sasl/data/modules/Custom Module/packs.lua
+++ plugins/sasl/data/modules/Custom Module/packs.lua
@@ -26,20 +26,20 @@ function onAirportLoaded()
 end
 
 function update()
-    --create the A321 pack system
-    set(Left_pack_iso_valve, 1)
-    set(Pack_M, 0)
+    --create the A321 pack system--
+    set(Left_pack_iso_valve, 1)--keeping the left iso closed which creates a single iso system
+    set(Pack_M, 0)--turning the center pack off as A320 doesn't have one
 
     --HP valve logic--
-    if get(Engine_1_avail) == 1 or get(Engine_mode_knob) == 1 or get(Engine_mode_knob) == -1 then
+    if get(Engine_1_avail) == 1 or get(Engine_mode_knob) == 1 or get(Engine_mode_knob) == -1 then--if eng 1 is running or engine mode is crank or ign
         set(L_HP_valve, 0)
-    else
+    else--if eng 1 is not and engine mode is norm
         set(L_HP_valve, 1)
     end
 
-    if get(Engine_2_avail) == 1 or get(Engine_mode_knob) == 1 or get(Engine_mode_knob) == -1 then
+    if get(Engine_2_avail) == 1 or get(Engine_mode_knob) == 1 or get(Engine_mode_knob) == -1 then--if eng 2 is running or engine mode is crank or ign
         set(R_HP_valve, 0)
-    else
+    else--if eng 2 is not and engine mode is norm
         set(R_HP_valve, 1)
     end
 
@@ -62,22 +62,45 @@ function update()
         set(Right_pack_iso_valve, 0)
     end
 
+    --X bleed bridge logic
+    if get(Apu_avail) == 1 and get(X_bleed_valve) == 0 then
+        set(X_bleed_bridge_state, 1)--bridged but closed
+    else
+        if get(X_bleed_valve) == 1 then
+            set(X_bleed_bridge_state, 2)--bridged and open
+        else
+            set(X_bleed_bridge_state, 0)--not bridged and closed
+        end
+    end
+
     --bleed logic--
-    if get(Engine_1_avail) == 1 then
-        set(ENG_1_bleed_switch, 1)
-    else
-        set(ENG_1_bleed_switch, 0)
+    if get(Engine_1_avail) == 1 then--engine 1 is running
+        set(ENG_1_bleed_switch, 1)--l bleed on
+        set(L_bleed_state, 2)
+        if get(Eng1_bleed_off) == 1 then--l bleed manually switched off
+            set(ENG_1_bleed_switch, 0)--l bleed off
+            set(L_bleed_state, 1)
+        end
+    else--engine 1 is not running
+        set(ENG_1_bleed_switch, 0)--l bleed off
+        set(L_bleed_state, 0)
     end
 
-    if get(Engine_2_avail) == 1 then
-        set(ENG_2_bleed_switch, 1)
-    else
-        set(ENG_2_bleed_switch, 0)
+    if get(Engine_2_avail) == 1 then--engine 2 is running
+        set(ENG_2_bleed_switch, 1)--r bleed on
+        set(R_bleed_state, 2)
+        if get(Eng2_bleed_off) == 1 then--l bleed manually switched off
+            set(ENG_2_bleed_switch, 0)--r bleed off
+            set(L_bleed_state, 1)
+        end
+    else--engine 2 is not running
+        set(ENG_2_bleed_switch, 0)--r bleed off
+        set(R_bleed_state, 0)
     end
 
-    if get(Apu_bleed_switch) == 1 and get(Apu_avail) == 1 then
-        set(ENG_1_bleed_switch, 0)
-        set(ENG_2_bleed_switch, 0)
+    if get(Apu_bleed_switch) == 1 and get(Apu_avail) == 1 then--apu is on and apu bleed is on
+        set(ENG_1_bleed_switch, 0)--l bleed off
+        set(ENG_2_bleed_switch, 0)--r bleed off
     end
 
     --PACKs logic--
@@ -211,30 +234,30 @@ function update()
     end
 
     --bleed temp--
-    if get(Left_bleed_avil) > 0.1 then
-        if get(Left_bleed_avil) > 1.2 then--engine bleed
-            set(L_bleed_press, Set_anim_value(get(L_bleed_press), 44, 0, 50, 0.35))
-            set(L_bleed_temp, Set_anim_value(get(L_bleed_temp), 190, -100, 200, 0.35))
+    if get(Left_bleed_avil) > 0.1 then--left side has bleed air and is in the apu bleed range
+        if get(Left_bleed_avil) > 1.2 then--left side has bleed air and is in the eng bleed range
+            set(L_bleed_press, Set_anim_value(get(L_bleed_press), 44, 0, 50, 0.35))--eng bleed with 44PSI
+            set(L_bleed_temp, Set_anim_value(get(L_bleed_temp), 190, -100, 200, 0.35))--eng bleed with 190C
         else--apu bleed
-            set(L_bleed_press, Set_anim_value(get(L_bleed_press), 39, 0, 50, 0.35))
-            set(L_bleed_temp, Set_anim_value(get(L_bleed_temp), 150, -100, 200, 0.35))
+            set(L_bleed_press, Set_anim_value(get(L_bleed_press), 39, 0, 50, 0.35))--apu bleed with 39PSI
+            set(L_bleed_temp, Set_anim_value(get(L_bleed_temp), 150, -100, 200, 0.35))--apu bleed with 150C
         end
     else
-        set(L_bleed_press, Set_anim_value(get(L_bleed_press), 0, 0, 50, 0.35))
-        set(L_bleed_temp, Set_anim_value(get(L_bleed_temp), get(OTA), -100, 200, 0.35))
+        set(L_bleed_press, Set_anim_value(get(L_bleed_press), 0, 0, 50, 0.35))--no bleed with 0PSI
+        set(L_bleed_temp, Set_anim_value(get(L_bleed_temp), get(OTA), -100, 200, 0.35))--no bleed with outside temp
     end
 
-    if get(Right_bleed_avil) > 0.1 then
-        if get(Right_bleed_avil) > 1.2 then--engine bleed
-            set(R_bleed_press, Set_anim_value(get(R_bleed_press), 44, 0, 50, 0.35))
-            set(R_bleed_temp, Set_anim_value(get(R_bleed_temp), 190, -100, 200, 0.35))
+    if get(Right_bleed_avil) > 0.1 then--right side has bleed air and is in the apu bleed range
+        if get(Right_bleed_avil) > 1.2 then--right side has bleed air and is in the eng bleed range
+            set(R_bleed_press, Set_anim_value(get(R_bleed_press), 44, 0, 50, 0.35))--eng bleed with 44PSI
+            set(R_bleed_temp, Set_anim_value(get(R_bleed_temp), 190, -100, 200, 0.35))--eng bleed with 190C
         else--apu bleed
-            set(R_bleed_press, Set_anim_value(get(R_bleed_press), 39, 0, 50, 0.35))
-            set(R_bleed_temp, Set_anim_value(get(R_bleed_temp), 150, -100, 200, 0.35))
+            set(R_bleed_press, Set_anim_value(get(R_bleed_press), 39, 0, 50, 0.35))--apu bleed with 39PSI
+            set(R_bleed_temp, Set_anim_value(get(R_bleed_temp), 150, -100, 200, 0.35))--apu bleed with 150C
         end
     else
-        set(R_bleed_press, Set_anim_value(get(R_bleed_press), 0, 0, 50, 0.35))
-        set(R_bleed_temp, Set_anim_value(get(R_bleed_temp), get(OTA), -100, 200, 0.35))
+        set(R_bleed_press, Set_anim_value(get(R_bleed_press), 0, 0, 50, 0.35))--no bleed with 0PSI
+        set(R_bleed_temp, Set_anim_value(get(R_bleed_temp), get(OTA), -100, 200, 0.35))--no bleed with outside temp
     end
 
 end
