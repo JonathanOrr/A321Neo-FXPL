1b79aa81ce0c5deba1c8cf550f3667389a8b2ffa
Chai <sirdesignpie@gmail.com>
2020-07-09 23:07:39 +0700

    update font

--- plugins/sasl/data/modules/Custom Module/MCDU.lua
+++ plugins/sasl/data/modules/Custom Module/MCDU.lua
@@ -61,7 +61,7 @@ local MCDU_DISP_TEXT_ALIGN =
 }
 
 --fonts
-local B612MONO_regular = sasl.gl.loadFont("fonts/B612Mono-Regular.ttf")
+local B612MONO_regular = sasl.gl.loadFont("fonts/A320PanelFont_V0.2b.ttf")
 
 -- alphanumeric & decimal FMC entry keys
 local MCDU_ENTRY_KEYS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "Δ"}
