129458dfaa20a33957a590bf85af4dbc1b2cf54c
JonathanOrr <61325694+JonathanOrr@users.noreply.github.com>
2020-07-30 22:49:45 +1000

    glareshield update

--- plugins/sasl/data/modules/Custom Module/cockpit_datarefs.lua
+++ plugins/sasl/data/modules/Custom Module/cockpit_datarefs.lua
@@ -28,3 +28,4 @@ Eng2_bleed_off = createGlobalPropertyi("a321neo/cockpit/packs/eng2_off", 0, fals
 --misc
 Capt_ra_alt_m = createGlobalPropertyf("a321neo/cockpit/indicators/capt_ra_alt_m", 0, false, true, false)
 Capt_baro_alt_m = createGlobalPropertyf("a321neo/cockpit/indicators/capt_baro_alt_m", 0, false, true, false)
+Window_ice_test = createGlobalPropertyf("a321neo/cockpit/window_ice", 0, false, true, false)
