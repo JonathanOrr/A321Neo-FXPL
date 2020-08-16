9bf0cddc89318128dbed855e9ff348acc19d51e9
JonathanOrr <61325694+JonathanOrr@users.noreply.github.com>
2020-08-16 22:55:34 +1000

    prepare for G hold

--- plugins/sasl/data/modules/Custom Module/FBW.lua
+++ plugins/sasl/data/modules/Custom Module/FBW.lua
@@ -7,12 +7,11 @@ local A32nx_FBW_roll_left_no_stick =  {P_gain = 1, D_gain = 25, Current_error = 
 local A32nx_FBW_roll_right_no_stick = {P_gain = 1, D_gain = 25, Current_error = 0, Min_error = -5, Max_error = 5, Error_offset = 0}
 local A32nx_FBW_pitch_up =   {P_gain = 1, D_gain = 10, Current_error = 0, Min_error = -5, Max_error = 5, Error_offset = 0}
 local A32nx_FBW_pitch_down = {P_gain = 1, D_gain = 10, Current_error = 0, Min_error = -5, Max_error = 5, Error_offset = 0}
-local A32nx_FBW_pitch_rate_up =   {P_gain = 1, D_gain = 10, Current_error = 0, Min_error = -0.5, Max_error = 0.5, Error_offset = 0}
-local A32nx_FBW_pitch_rate_down =   {P_gain = 1, D_gain = 10, Current_error = 0, Min_error = -0.5, Max_error = 0.5, Error_offset = 0}
 local A32nx_FBW_roll_rate_command = {P_gain = 0.8, I_gain = 1, D_gain = 2, I_delay = 120, Integral = 0, Current_error = 0, Min_error = -1, Max_error = 1, Error_offset = 0}
 local A32nx_FBW_0pitch_command = {P_gain = 150, I_gain = 10, D_gain = 1000, I_delay = 120, Integral = 0, Current_error = 0, Min_error = -0.15, Max_error = 0.15, Error_offset = 0}
 local A32nx_FBW_FPS_dep_0vpath = {P_gain = 2.45, I_gain = 5.6, D_gain = 300, I_delay = 60, Integral = 0, Current_error = 0, Min_error = -0.1, Max_error = 0.1, Error_offset = 0}
 local A32nx_FBW_FPS_indep_0vpath = {P_gain = 2.6, I_gain = 5.8, D_gain = 320, I_delay = 75, Integral = 0, Current_error = 0, Min_error = -22.5, Max_error = 22.5, Error_offset = 0}
+local A32nx_FBW_1G_command = {P_gain = 2.6, I_gain = 5.8, D_gain = 320, I_delay = 75, Integral = 0, Current_error = 0, Min_error = -22.5, Max_error = 22.5, Error_offset = 0}
 local A32nx_FBW_G_command = {P_gain = 0.16, I_gain = 1, D_gain = 7.8, I_delay = 120, Integral = 0, Current_error = 0, Min_error = -0.15, Max_error = 0.15, Error_offset = 0}
 local A32nx_FBW_AOA_protection = {P_gain = 1, I_gain = 1, D_gain = 10, I_delay = 120, Integral = 0, Current_error = 0, Min_error = -5, Max_error = 5, Error_offset = 0}
 local A32nx_FBW_MAX_spd_protection = {P_gain = 1, I_gain = 1, D_gain = 10, I_delay = 120, Integral = 0, Current_error = 0, Min_error = -5, Max_error = 5, Error_offset = 0}
@@ -21,12 +20,12 @@ local A32nx_FBW_pitch_elev_trim = {P_gain = 1, I_gain = 1, D_gain = 10, I_delay 
 local A32nx_FBW_vpath_elev_trim = {P_gain = 2.45, I_gain = 5.8, D_gain = 240, I_delay = 60, Integral = 0, Current_error = 0, Min_error = -0.15, Max_error = 0.15, Error_offset = 0}
 
 --datarefs for live tunning
-local live_P_gain = createGlobalPropertyf("a321neo/debug/pid/live_p_gain", 2.6, false, true, false)
-local live_I_gain = createGlobalPropertyf("a321neo/debug/pid/live_I_gain", 5.8, false, true, false)
-local live_D_gain = createGlobalPropertyf("a321neo/debug/pid/live_d_gain", 320, false, true, false)
-local live_max_lim = createGlobalPropertyf("a321neo/debug/pid/live_max_lim", 22.5, false, true, false)
-local live_min_lim = createGlobalPropertyf("a321neo/debug/pid/live_min_lim", -22.5, false, true, false)
-local live_curve_speed = createGlobalPropertyf("a321neo/debug/pid/live_curve_speed", 0.8, false, true, false)
+local live_P_gain = createGlobalPropertyf("a321neo/debug/pid/live_p_gain", 0.16, false, true, false)
+local live_I_gain = createGlobalPropertyf("a321neo/debug/pid/live_I_gain", 1, false, true, false)
+local live_D_gain = createGlobalPropertyf("a321neo/debug/pid/live_d_gain", 45, false, true, false)
+local live_max_lim = createGlobalPropertyf("a321neo/debug/pid/live_max_lim", 0.15, false, true, false)
+local live_min_lim = createGlobalPropertyf("a321neo/debug/pid/live_min_lim", -0.15, false, true, false)
+local live_curve_speed = createGlobalPropertyf("a321neo/debug/pid/live_curve_speed", 0.35, false, true, false)
 
 --[[establishing the FBW laws:
     NORMAL LAW: 30 UP, 15 DOWN, stick active: 67 LEFT & RIGHT, not active 30 LEFT & RIGHT, 14 degrees alpha, OVERSPEED protection, roll rate of 30, 2.5G to -1G in normal flight,2G to 0G if flaps down envelop, always CWS
@@ -164,6 +163,12 @@ end
 function update()
     set(Override_artstab, 1)
 
+    A32nx_FBW_1G_command.P_gain = get(live_P_gain)
+    A32nx_FBW_1G_command.I_gain = get(live_I_gain)
+    A32nx_FBW_1G_command.D_gain = get(live_D_gain)
+    A32nx_FBW_1G_command.Max_error = get(live_max_lim)
+    A32nx_FBW_1G_command.Min_error = get(live_min_lim)
+
     --calculate abs pitch rate
     set(Abs_pitch_rate, get(Flightmodel_pitch) - last_pitch)
     last_pitch = get(Flightmodel_pitch)
@@ -288,7 +293,9 @@ function update()
         if get(FBW_status) == 2 then
             if get(FBW_pitch_mode) == 2 then--hold persec vpath
                 --command 0 vpath pitch rate clamped to stop integral build up
-                set(Vpath_output, Set_anim_value(get(Vpath_output), Math_clamp(Math_clamp(FBW_PID(A32nx_FBW_FPS_indep_0vpath, 0 - get(Persec_vpath_pitch_rate)), -1, get(AOA_lim)+1), get(Pitch_d_lim), get(Pitch_u_lim)), -1, 1, 0.8))
+                --set(Vpath_output, Set_anim_value(get(Vpath_output), Math_clamp(Math_clamp(FBW_PID(A32nx_FBW_FPS_indep_0vpath, 0 - get(Persec_vpath_pitch_rate)), -1, get(AOA_lim)+1), get(Pitch_d_lim), get(Pitch_u_lim)), -1, 1, 0.8))
+                set(Vpath_output, Set_anim_value(get(Vpath_output), Math_clamp(Math_clamp(FBW_PID(A32nx_FBW_1G_command, 1 - get(Total_vertical_g_load)), -1, get(AOA_lim)+1), get(Pitch_d_lim), get(Pitch_u_lim)), -1, 1, get(live_curve_speed)))
+                --set(Vpath_output, Math_clamp(Math_clamp(FBW_PID_no_lim(A32nx_FBW_0G_command, 1 - get(Total_vertical_g_load)), -1, get(AOA_lim)+1), get(Pitch_d_lim), get(Pitch_u_lim)))
                 set(G_output, get(Vpath_output))
             elseif get(FBW_pitch_mode) == 1 then--hold abs vpath
                 set(Vpath_output, Set_anim_value(get(Vpath_output), Math_clamp(Math_clamp(FBW_PID(A32nx_FBW_FPS_dep_0vpath, 0 - get(Abs_vpath_pitch_rate)), -1, get(AOA_lim)+1), get(Pitch_d_lim), get(Pitch_u_lim)), -1, 1, 0.215))
