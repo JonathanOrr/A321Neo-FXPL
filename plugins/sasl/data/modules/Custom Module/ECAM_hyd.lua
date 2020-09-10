802c82d2ffa95aad0106fd6507ce7c84abe27b4f
Rico Rico <rico23rico@users.noreply.github.com>
2020-09-10 20:42:23 +0200

    [HYD] Implementation of basic graphic for B and Y

--- plugins/sasl/data/modules/Custom Module/ECAM_hyd.lua
+++ plugins/sasl/data/modules/Custom Module/ECAM_hyd.lua
@@ -7,15 +7,17 @@ local y_psi_pos = size[2]/2+265 -- Starting top point (the PSI numbers position)
 local function draw_psi_numbers(g_psi, b_psi, y_psi)
 
     -- GREEN
-    local g_psi = get(Hydraulic_G_press)
-    g_psi = g_psi - g_psi % 50
-    g_color = g_psi >= 1450 and ECAM_GREEN or ECAM_ORANGE
+    local g_color = g_psi >= 1450 and ECAM_GREEN or ECAM_ORANGE
     sasl.gl.drawText(B612MONO_regular, 160, y_psi_pos, g_psi, 36, false, false, TEXT_ALIGN_CENTER, g_color)
     sasl.gl.drawWideLine (160, y_psi_pos-20 , 160, y_psi_pos-240, 4 , g_color)
 
-    sasl.gl.drawText(B612MONO_regular, 455, y_psi_pos, b_psi, 36, false, false, TEXT_ALIGN_CENTER, b_psi >= 1450 and ECAM_GREEN or ECAM_ORANGE)
-    
-    sasl.gl.drawText(B612MONO_regular, 750, y_psi_pos, y_psi, 36, false, false, TEXT_ALIGN_CENTER, y_psi >= 1450 and ECAM_GREEN or ECAM_ORANGE)
+    local b_color = y_psi >= 1450 and ECAM_GREEN or ECAM_ORANGE
+    sasl.gl.drawText(B612MONO_regular, 453, y_psi_pos, b_psi, 36, false, false, TEXT_ALIGN_CENTER, b_color)
+    sasl.gl.drawWideLine (453, y_psi_pos-20 , 453, y_psi_pos-285, 4 , g_color)
+        
+    local y_color = y_psi >= 1450 and ECAM_GREEN or ECAM_ORANGE
+    sasl.gl.drawText(B612MONO_regular, 745, y_psi_pos, y_psi, 36, false, false, TEXT_ALIGN_CENTER, y_color)
+    sasl.gl.drawWideLine (745, y_psi_pos-20 , 745, y_psi_pos-240, 4 , g_color)
 end
 
 local function draw_rectangles(g_psi, b_psi, y_psi)
@@ -39,11 +41,32 @@ local function draw_rectangles(g_psi, b_psi, y_psi)
     ----------------------
     -- BLUE Rectangle   --
     ----------------------
-
+    color_rectangle = ECAM_ORANGE
+    if get(Hyd_light_B_ElecPump) % 10 == 1 then
+        sasl.gl.drawWideLine (453-35, y_psi_pos-335, 453+35, y_psi_pos-335, 4, ECAM_ORANGE)
+    elseif b_psi >= 1450 then
+        color_rectangle = ECAM_GREEN
+        sasl.gl.drawWideLine (453, y_psi_pos-335, 453, y_psi_pos-335+50, 4, ECAM_GREEN)
+    else
+        sasl.gl.drawText(B612MONO_regular, 452, y_psi_pos-325, "LO", 45, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    end
+    -- Draw the rectangle external
+    sasl.gl.drawWidePolyLine( {418, y_psi_pos-335, 418+70, y_psi_pos-335, 418+70, y_psi_pos-335+50, 418, y_psi_pos-335+50, 418, y_psi_pos-335 }, 4, color_rectangle)
 
     ----------------------
     -- YELLOW Rectangle --
     ----------------------
+    color_rectangle = ECAM_ORANGE
+    if get(Hyd_light_Eng2Pump) % 10 == 1 then
+        sasl.gl.drawWideLine (745-35, y_psi_pos-265 , 745+35, y_psi_pos-265, 4, ECAM_ORANGE)
+    elseif y_psi >= 1450 then
+        color_rectangle = ECAM_GREEN
+        sasl.gl.drawWideLine (745, y_psi_pos-290, 745, y_psi_pos-290+50, 4, ECAM_GREEN)
+    else
+        sasl.gl.drawText(B612MONO_regular, 745, y_psi_pos-280, "LO", 45, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    end
+    -- Draw the rectangle external
+    sasl.gl.drawWidePolyLine( {710, y_psi_pos-290, 710+70, y_psi_pos-290, 710+70, y_psi_pos-290+50, 710, y_psi_pos-290+50, 710, y_psi_pos-290 }, 4, color_rectangle)
     
 end
 
@@ -72,10 +95,37 @@ local function draw_quantity_bars(qty_G, qty_B, qty_Y)
 end
 
 local function draw_failures()
-    sasl.gl.drawText(B612MONO_regular, 270, size[2]/2-185, "LO AIR", 34, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
-    sasl.gl.drawText(B612MONO_regular, 270, size[2]/2-230, "PRESS", 34, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)        
-    sasl.gl.drawText(B612MONO_regular, 270, size[2]/2-310, "OVHT", 45, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    sasl.gl.drawText(B612MONO_regular, 250, size[2]/2-185, "LO AIR", 32, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    sasl.gl.drawText(B612MONO_regular, 250, size[2]/2-230, "PRESS", 32, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)        
+    sasl.gl.drawText(B612MONO_regular, 250, size[2]/2-310, "OVHT", 42, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
 
+    sasl.gl.drawText(B612MONO_regular, 540, size[2]/2-185, "LO AIR", 32, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    sasl.gl.drawText(B612MONO_regular, 540, size[2]/2-230, "PRESS", 32, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)        
+    sasl.gl.drawText(B612MONO_regular, 540, size[2]/2-310, "OVHT", 42, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    
+    sasl.gl.drawText(B612MONO_regular, 830, size[2]/2-185, "LO AIR", 32, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    sasl.gl.drawText(B612MONO_regular, 830, size[2]/2-230, "PRESS", 32, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)        
+    sasl.gl.drawText(B612MONO_regular, 830, size[2]/2-310, "OVHT", 42, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+end
+
+local function draw_extra_pumps()
+
+    -- Yellow ELEC pump
+    sasl.gl.drawText(B612MONO_regular, 840, size[2]/2+100, "ELEC", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+    sasl.gl.drawText(B612MONO_regular, 840, size[2]/2+70, "OVHT", 26, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+    
+    sasl.gl.drawWideLine ( 747, size[2]/2+95 , 772, size[2]/2+95, 4 , ECAM_ORANGE)
+    sasl.gl.drawTriangle ( 770 , size[2]/2+95 , 795 , size[2]/2+110 , 795 , size[2]/2+80 , ECAM_ORANGE )
+    
+    -- BLUE ELEC pump messages
+    sasl.gl.drawText(B612MONO_regular, 540, size[2]/2-10, "ELEC", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+    sasl.gl.drawText(B612MONO_regular, 540, size[2]/2-40, "OVHT", 26, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
+
+    -- RAT messages
+    sasl.gl.drawText(B612MONO_regular, 370, size[2]/2+85, "RAT", 26, false, false, TEXT_ALIGN_CENTER, ECAM_WHITE)
+    sasl.gl.drawWideLine ( 428, size[2]/2+95 , 453, size[2]/2+95, 4 , ECAM_GREEN)
+    sasl.gl.drawTriangle ( 432 , size[2]/2+95 , 407 , size[2]/2+110 , 407 , size[2]/2+80 , ECAM_GREEN )
+    
 end
 
 function draw_hydraulic_page()
@@ -100,6 +150,8 @@ function draw_hydraulic_page()
     local qty_Y = get(Hydraulic_Y_qty)
     
     draw_quantity_bars(qty_G, qty_B, qty_Y)
+
+    draw_extra_pumps()  -- Draw ELEC for Y and RAT for B
 
     draw_failures()
 
