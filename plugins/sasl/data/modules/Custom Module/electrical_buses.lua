f07adc0556e61feefd58af7080f046b033d7a162
Rico Rico <rico23rico@users.noreply.github.com>
2020-09-19 00:43:17 +0200

    [ELEC] Bus logic implementation

--- plugins/sasl/data/modules/Custom Module/electrical_buses.lua
+++ plugins/sasl/data/modules/Custom Module/electrical_buses.lua
@@ -6,12 +6,22 @@ local GEN_2 = 2
 local GEN_APU = 3
 local GEN_EXT  = 4
 local GEN_EMER = 5
-local GEN_FAKE_BUS_TIE = 99
 
 local AC_BUS_1 = 11
 local AC_BUS_2 = 12
 
 local STATIC_INVERTER = 21
+
+local TR_1 = 31
+local TR_2 = 32
+local TR_ESS = 33
+
+local BAT_1 = 41
+local BAT_2 = 42
+
+local CROSS_TIEBAT_BUS = 98
+local GEN_FAKE_BUS_TIE = 99
+
 ----------------------------------------------------------------------------------------------------
 -- Commands
 ----------------------------------------------------------------------------------------------------
@@ -27,7 +37,15 @@ local ac_ess_bus_pushbutton_status = false  -- true: alternate, false: normal
 buses = {
     ac1_powered_by = 0,
     ac2_powered_by = 0,
-    ac_ess_powered_by = 0
+    ac_ess_powered_by = 0,
+    dc1_powered_by = 0,
+    dc2_powered_by = 0,
+    dc_ess_powered_by = 0,
+    dc_bat_bus_powered_by = 0,
+    
+    is_ac_ess_shed_on = 0,
+    is_dc_ess_shed_on = 0
+    
 }
 
 ELEC_sys.buses = buses
@@ -106,21 +124,96 @@ local function update_ac_ess()
     
 end
 
+local function update_dc_1()
+
+    buses.dc1_powered_by  = 0
+
+    -- The order of this if-elseif matches the real source priority! Do not change it!
+
+    if get(TR_1_online) == 1 then
+        buses.dc1_powered_by = TR_1
+    elseif buses.dc2_powered_by > 0 and buses.dc2_powered_by ~= CROSS_TIEBAT_BUS then
+        buses.dc1_powered_by = CROSS_TIEBAT_BUS
+    end
+
+end
+
+local function update_dc_2()
+
+    buses.dc2_powered_by  = 0
+
+    -- The order of this if-elseif matches the real source priority! Do not change it!
+
+    if get(TR_2_online) == 1 then
+        buses.dc2_powered_by = TR_2
+    elseif buses.dc1_powered_by > 0 and buses.dc1_powered_by ~= CROSS_TIEBAT_BUS then
+        buses.dc2_powered_by = CROSS_TIEBAT_BUS
+    end
+
+end
+
+local function update_dc_ess()
+    buses.dc_ess_powered_by  = 0
+
+    -- The order of this if-elseif matches the real source priority! Do not change it!
+
+    if get(TR_1_online) == 1 then
+        buses.dc_ess_powered_by = TR_1
+    elseif get(TR_ESS_online) == 1 then
+        buses.dc_ess_powered_by = TR_ESS
+    elseif get(HOT_bus_2_pwrd) and batteries[2].switch_status == true then
+        buses.dc_ess_powered_by = BAT_2
+    end
+end
+
+local function update_dc_bat_bus()
+    buses.dc_bat_bus_powered_by  = 0
+
+    -- The order of this if-elseif matches the real source priority! Do not change it!
+
+    if get(TR_1_online) == 1 then
+        buses.dc_bat_bus_powered_by = TR_1
+    elseif get(TR_2_online) == 1 then
+        buses.dc_bat_bus_powered_by = TR_2
+    end
+end
+
 local function update_datarefs()
 
     set(AC_bus_1_pwrd, buses.ac1_powered_by > 0 and 1 or 0)
     set(AC_bus_2_pwrd, buses.ac2_powered_by > 0 and 1 or 0)
     set(AC_ess_bus_pwrd, buses.ac_ess_powered_by > 0 and 1 or 0)
     
+    set(DC_bus_1_pwrd, buses.dc1_powered_by > 0 and 1 or 0)
+    set(DC_bus_2_pwrd, buses.dc2_powered_by > 0 and 1 or 0)
+    set(DC_ess_bus_pwrd, buses.dc_ess_powered_by > 0 and 1 or 0)
+    set(DC_bat_bus_pwrd, buses.dc_bat_bus_powered_by > 0 and 1 or 0)
+
+    set(AC_ess_shed_pwrd, buses.is_ac_ess_shed_on and 1 or 0)
+    set(DC_shed_ess_pwrd, buses.is_dc_ess_shed_on and 1 or 0)
+
+
     set(Elec_light_BUS_tie, bus_tie_pushbutton_status and 0 or 1)
     set(Elec_light_AC_ess_feed, (ac_ess_bus_pushbutton_status and 1 or 0) + (buses.ac_ess_powered_by>0 and 0 or 10)) 
+end
+
+local function update_shed()
+    buses.is_ac_ess_shed_on = buses.ac1_powered_by > 0 or buses.ac2_powered_by > 0
+                              or buses.ac_ess_powered_by == GEN_EMER
+    buses.is_dc_ess_shed_on = buses.dc1_powered_by > 0 or buses.dc2_powered_by > 0 
+                              or buses.dc_ess_powered_by == TR_ESS
 end
 
 function update_buses()
     update_ac_1()
     update_ac_2()
     update_ac_ess()
-    
+    update_dc_1()
+    update_dc_2()
+    update_dc_ess()
+    update_dc_bat_bus()
+    update_shed()
+
     update_datarefs()
     
 end
