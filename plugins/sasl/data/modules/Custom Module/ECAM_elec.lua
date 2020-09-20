3bd7c4bd9ce76c240365721b3eaa770e9515ec9f
Rico Rico <rico23rico@users.noreply.github.com>
2020-09-20 18:57:53 +0200

    [ECAM/ELEC] Lines and other stuffs on the ELEC ecam page

--- plugins/sasl/data/modules/Custom Module/ECAM_elec.lua
+++ plugins/sasl/data/modules/Custom Module/ECAM_elec.lua
@@ -1,10 +1,40 @@
 size = {900, 900}
 include('constants.lua')
 
+local GEN_1 = 1
+local GEN_2 = 2
+local GEN_APU = 3
+local GEN_EXT  = 4
+local GEN_EMER = 5
+
+local AC_BUS_1 = 11
+local AC_BUS_2 = 12
+
+local STATIC_INVERTER = 21
+
+local TR_1 = 31
+local TR_2 = 32
+local TR_ESS = 33
+
+local BAT_1 = 41
+local BAT_2 = 42
+
+local CROSS_TIEBAT_BUS = 98
+local GEN_FAKE_BUS_TIE = 99
+
+
 local GEN_ENGINE_RATED_CURR = 261    -- Maximum current provided by the engine gen (this is not enforced but used to compute the load %)
 local GEN_ENGINE_APU_CURR   = 261    -- Maximum current provided by the APU gen (this is not enforced but used to compute the load %)
 
 local B612MONO_regular = sasl.gl.loadFont("fonts/B612Mono-Regular.ttf")
+
+local function draw_open_arrow_up(x,y,color)
+    sasl.gl.drawWidePolyLine( {x, y, x-10, y-15, x+10, y-15, x, y }, 3, color)
+end
+local function draw_open_arrow_down(x,y,color)
+    sasl.gl.drawWidePolyLine( {x, y, x-10, y+15, x+10, y+15, x, y }, 3, color)
+end
+
 
 local function update_draw_datarefs()
     set(Ecam_elec_bat_1_status, ELEC_sys.batteries[1].switch_status and 1 or 0)
@@ -23,7 +53,7 @@ local function update_draw_datarefs()
         set(Ecam_elec_apu_gen_status, 3)    
     end
 
-   if ELEC_sys.generators[5].source_status == false then
+   if ELEC_sys.generators[5].switch_status == false then
         set(Ecam_elec_rat_status, 0)
     elseif ELEC_sys.generators[5].curr_voltage > 105 and ELEC_sys.generators[5].curr_hz > 385 then
         set(Ecam_elec_rat_status, 1)
@@ -50,7 +80,7 @@ local function draw_battery_contactor(i,x)
     
     local curr_amps = ELEC_sys.batteries[i].curr_source_amps-ELEC_sys.batteries[i].curr_sink_amps
     
-    if curr_amps < -1 then
+    if curr_amps > 11 then
         sasl.gl.drawWideLine(x_start, y, x_end, y, 3, ECAM_ORANGE)
         if i == 1 then
             sasl.gl.drawTriangle (x_end-15, y+10 , x_end-15, y-10, x_end+1, y, ECAM_ORANGE)
@@ -61,7 +91,7 @@ local function draw_battery_contactor(i,x)
         sasl.gl.drawWideLine(x_start, y, x_end, y, 3, ECAM_GREEN)
     end
     
-    if curr_amps > 1 then
+    if curr_amps < -1 then
         if i == 1 then
             sasl.gl.drawTriangle (x_start+15, y+10 , x_start+15, y-10, x_start-1, y, ECAM_GREEN)
         else
@@ -75,7 +105,7 @@ local function draw_battery(i, x)
     local curr_amps = ELEC_sys.batteries[i].curr_sink_amps-ELEC_sys.batteries[i].curr_source_amps
     local failed_bat = (i == 1 and get(FAILURE_ELEC_battery_1) == 1) or (i == 2 and get(FAILURE_ELEC_battery_2) == 1)
     local bat_1_status = (ELEC_sys.batteries[i].curr_voltage < 25 or ELEC_sys.batteries[i].curr_voltage > 31 or
-                         curr_amps < -5 or failed_bat) and ELEC_sys.batteries[i].switch_status
+                         curr_amps > 5 or failed_bat) and ELEC_sys.batteries[i].switch_status
 
     sasl.gl.drawText(B612MONO_regular, x, size[2]/2+395, "BAT " .. i, 26, false, false, 
                      TEXT_ALIGN_LEFT, bat_1_status and ECAM_ORANGE or ECAM_WHITE)
@@ -115,8 +145,8 @@ local function draw_tr(i, x)
                      ELEC_sys.trs[i].curr_out_amps <= 5 and ECAM_ORANGE or ECAM_GREEN)
     
     if ELEC_sys.trs[i].curr_voltage >= 25 then
-        sasl.gl.drawWideLine(x+55, size[2]/2+201, x+55, size[2]/2+280, 3, ECAM_GREEN)
-        sasl.gl.drawWideLine(x+55, size[2]/2+92, x+55, size[2]/2+38, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(x+50, size[2]/2+201, x+50, size[2]/2+280, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(x+50, size[2]/2+92, x+50, size[2]/2+38, 3, ECAM_GREEN)
     end
     
 end
@@ -124,6 +154,7 @@ end
 local function draw_ess_tr()
     local failed_tr = ELEC_sys.trs[3].curr_voltage < 25 or ELEC_sys.trs[3].curr_voltage > 31 or ELEC_sys.trs[3].curr_out_amps <= 5
     
+
     if get(Ecam_elec_tr_ess_status) == 0 then
         return -- Nothing to draw it's hidden
     end
@@ -138,10 +169,12 @@ local function draw_ess_tr()
                      TEXT_ALIGN_RIGHT,
                      ELEC_sys.trs[3].curr_out_amps <= 5 and ECAM_ORANGE or ECAM_GREEN)
     
+    sasl.gl.drawWideLine(400, size[2]/2+82, 400, size[2]/2+26, 3, ECAM_GREEN)
     
 end
 
 local function draw_apu_gen()
+
     if get(Ecam_elec_apu_gen_status) <= 1 then
         return  -- Nothing to do
     end
@@ -151,7 +184,6 @@ local function draw_apu_gen()
                      TEXT_ALIGN_RIGHT,
                      (-ELEC_sys.generators[3].curr_amps > GEN_ENGINE_APU_CURR)
                      and ECAM_ORANGE or ECAM_GREEN)
-
     
     sasl.gl.drawText(B612MONO_regular, 335, size[2]/2-215, 
                      math.floor(ELEC_sys.generators[3].curr_voltage+0.5), 28, false, false,
@@ -164,6 +196,44 @@ local function draw_apu_gen()
                      TEXT_ALIGN_RIGHT,
                      (ELEC_sys.generators[3].curr_hz < 390 or ELEC_sys.generators[3].curr_hz > 410)
                      and ECAM_ORANGE or ECAM_GREEN)
+                     
+    -- Draw lines
+    if ELEC_sys.buses.ac1_powered_by == 3 then
+        draw_open_arrow_up(80,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(80, size[2]/2-40, 80, size[2]/2-5, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(320, size[2]/2-40, 80, size[2]/2-40, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(320, size[2]/2-40, 320, size[2]/2-104, 3, ECAM_GREEN)      
+        draw_open_arrow_up(320,size[2]/2-104,ECAM_GREEN)  
+    end
+    
+    if ELEC_sys.buses.ac2_powered_by == 3 then
+        draw_open_arrow_up(820,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(820, size[2]/2-40, 820, size[2]/2-5, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(320, size[2]/2-40, 820, size[2]/2-40, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(320, size[2]/2-40, 320, size[2]/2-104, 3, ECAM_GREEN)
+        draw_open_arrow_up(320,size[2]/2-104,ECAM_GREEN)  
+    end
+end
+
+local function draw_gen_lines(x,i)
+    if (i == 1 and ELEC_sys.buses.ac1_powered_by == 1) or (i == 2 and ELEC_sys.buses.ac2_powered_by == 2) then
+        draw_open_arrow_up(x-30,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(x-30, size[2]/2-81, x-30, size[2]/2-5, 3, ECAM_GREEN)
+    end
+    
+    if i == 1 and ELEC_sys.buses.ac2_powered_by == 99 then
+        draw_open_arrow_up(820,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(820, size[2]/2-40, 820, size[2]/2-5, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(80, size[2]/2-40, 820, size[2]/2-40, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(80, size[2]/2-40, 80, size[2]/2-81, 3, ECAM_GREEN)
+    end
+
+    if i == 2 and ELEC_sys.buses.ac1_powered_by == 99 then
+        draw_open_arrow_up(80,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(80, size[2]/2-40, 80, size[2]/2-5, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(820, size[2]/2-40, 80, size[2]/2-40, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(820, size[2]/2-40, 820, size[2]/2-81, 3, ECAM_GREEN)
+    end
 end
 
 local function draw_gen(x, i)
@@ -196,6 +266,8 @@ local function draw_gen(x, i)
                      (ELEC_sys.generators[i].curr_hz < 390 or ELEC_sys.generators[i].curr_hz > 410)
                      and ECAM_ORANGE or ECAM_GREEN)
 
+    -- Draw lines if they are powering the AC bus
+    draw_gen_lines(x, i)
 end
 
 local function draw_stat_inv()
@@ -215,6 +287,121 @@ local function draw_stat_inv()
                      (ELEC_sys.stat_inv.curr_hz < 390 or ELEC_sys.stat_inv.curr_hz > 410)
                      and ECAM_ORANGE or ECAM_GREEN)
     
+    draw_open_arrow_down(240,size[2]/2+301,ECAM_GREEN)
+    sasl.gl.drawWideLine(240, size[2]/2+301, 240, size[2]/2-75, 3, ECAM_GREEN)
+    sasl.gl.drawWideLine(240, size[2]/2-75, 525, size[2]/2-75, 3, ECAM_GREEN)
+end
+
+local function draw_emer_gen()
+    if not ELEC_sys.generators[5].switch_status then
+        return  -- Nothing to do
+    end
+    
+    sasl.gl.drawText(B612MONO_regular, 585, size[2]/2+125, 
+                     math.floor(ELEC_sys.generators[5].curr_voltage+0.5), 28, false, false,
+                     TEXT_ALIGN_RIGHT,
+                     (ELEC_sys.generators[5].curr_voltage < 110 or ELEC_sys.generators[5].curr_voltage > 120)
+                     and ECAM_ORANGE or ECAM_GREEN)
+
+    sasl.gl.drawText(B612MONO_regular, 585, size[2]/2+95, 
+                     math.floor(ELEC_sys.generators[5].curr_hz+0.5), 28, false, false,
+                     TEXT_ALIGN_RIGHT,
+                     (ELEC_sys.generators[5].curr_hz < 390 or ELEC_sys.generators[5].curr_hz > 410)
+                     and ECAM_ORANGE or ECAM_GREEN)
+    
+end
+
+local function draw_ext_pwr()
+    if not ELEC_sys.generators[GEN_EXT].switch_status then
+        return  -- Nothing to do
+    end
+    
+    sasl.gl.drawText(B612MONO_regular, 585, size[2]/2-218, 
+                     math.floor(ELEC_sys.generators[4].curr_voltage+0.5), 28, false, false,
+                     TEXT_ALIGN_RIGHT,
+                     (ELEC_sys.generators[GEN_EXT].curr_voltage < 110 or ELEC_sys.generators[GEN_EXT].curr_voltage > 120)
+                     and ECAM_ORANGE or ECAM_GREEN)
+
+    sasl.gl.drawText(B612MONO_regular, 585, size[2]/2-248, 
+                     math.floor(ELEC_sys.generators[GEN_EXT].curr_hz+0.5), 28, false, false,
+                     TEXT_ALIGN_RIGHT,
+                     (ELEC_sys.generators[GEN_EXT].curr_hz < 390 or ELEC_sys.generators[GEN_EXT].curr_hz > 410)
+                     and ECAM_ORANGE or ECAM_GREEN)
+    
+    if ELEC_sys.buses.ac1_powered_by == GEN_EXT then
+        draw_open_arrow_up(80,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(80, size[2]/2-40, 80, size[2]/2-5, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(570, size[2]/2-40, 80, size[2]/2-40, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(570, size[2]/2-40, 570, size[2]/2-132, 3, ECAM_GREEN)
+        draw_open_arrow_up(570,size[2]/2-132,ECAM_GREEN)
+    end
+    
+    if ELEC_sys.buses.ac2_powered_by == GEN_EXT then
+        draw_open_arrow_up(820,size[2]/2+10,ECAM_GREEN)
+        sasl.gl.drawWideLine(820, size[2]/2-40, 820, size[2]/2-5, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(570, size[2]/2-40, 820, size[2]/2-40, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(570, size[2]/2-40, 570, size[2]/2-132, 3, ECAM_GREEN)
+        draw_open_arrow_up(570,size[2]/2-132,ECAM_GREEN)
+    end             
+    
+end
+
+local function draw_ess_bus_lines()
+
+    -- AC
+    if ELEC_sys.buses.ac_ess_powered_by == AC_BUS_1 then
+        sasl.gl.drawWideLine(211, size[2]/2+20, 355, size[2]/2+20, 3, ECAM_GREEN)
+    end
+    if ELEC_sys.buses.ac_ess_powered_by == AC_BUS_2 then
+        sasl.gl.drawWideLine(560, size[2]/2+20, 688, size[2]/2+20, 3, ECAM_GREEN)
+    end
+    if ELEC_sys.buses.ac_ess_powered_by == GEN_EMER then
+        draw_open_arrow_down(540, size[2]/2+27, ECAM_GREEN)
+        sasl.gl.drawWideLine(540, size[2]/2+42, 540, size[2]/2+82, 3, ECAM_GREEN)
+    end
+    if ELEC_sys.buses.ac_ess_powered_by == STATIC_INVERTER then
+        draw_open_arrow_up(550,size[2]/2-1,ECAM_GREEN)
+        sasl.gl.drawWideLine(550, size[2]/2-16, 550, size[2]/2-48, 3, ECAM_GREEN)
+    end
+
+    -- DC
+    if ELEC_sys.buses.dc_ess_powered_by == TR_ESS then
+        sasl.gl.drawWideLine(380, size[2]/2+238, 380, size[2]/2+209, 3, ECAM_GREEN)
+        draw_open_arrow_up(380, size[2]/2+209, ECAM_GREEN)  
+    elseif ELEC_sys.buses.dc_ess_powered_by == TR_1 then
+        sasl.gl.drawWideLine(380, size[2]/2+265, 380, size[2]/2+295, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(380, size[2]/2+295, 138, size[2]/2+295, 3, ECAM_GREEN)
+    elseif ELEC_sys.buses.dc_ess_powered_by == BAT_2 then
+        sasl.gl.drawWideLine(556, size[2]/2+251, 650, size[2]/2+251, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(650, size[2]/2+251, 650, size[2]/2+297, 3, ECAM_GREEN)
+        draw_open_arrow_down(650, size[2]/2+297, ECAM_GREEN)    
+    end
+
+end
+
+local function draw_shed_legends()
+
+    if get(DC_shed_ess_pwrd) == 0 then
+        sasl.gl.drawText(B612MONO_regular, 450, size[2]/2+213,
+                         "SHED", 24, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE )
+    
+    end
+    if get(AC_ess_shed_pwrd) == 0 then
+        sasl.gl.drawText(B612MONO_regular, 450, size[2]/2-28, 
+                         "SHED", 24, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE )
+    end
+    
+end
+
+local function draw_bat_dc_bus_lines()
+
+    if ELEC_sys.buses.dc_bat_bus_powered_by == TR_1 then
+        sasl.gl.drawWideLine(380, size[2]/2+359, 380, size[2]/2+295, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(380, size[2]/2+295, 138, size[2]/2+295, 3, ECAM_GREEN)
+    elseif ELEC_sys.buses.dc_bat_bus_powered_by == TR_2 then
+        sasl.gl.drawWideLine(530, size[2]/2+359, 530, size[2]/2+295, 3, ECAM_GREEN)
+        sasl.gl.drawWideLine(530, size[2]/2+295, 761, size[2]/2+295, 3, ECAM_GREEN)
+    end
 end
 
 function draw_elec_page()
@@ -233,6 +420,10 @@ function draw_elec_page()
     draw_gen(850, 2)
 
     draw_stat_inv()
+    draw_emer_gen()
+    draw_ext_pwr()
 
-
+    draw_shed_legends()
+    draw_ess_bus_lines()
+    draw_bat_dc_bus_lines()
 end
