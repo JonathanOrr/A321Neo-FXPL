ba3d07c0d1444e3ccb63834a187df1718137d3a3
Rico Rico <rico23rico@users.noreply.github.com>
2020-10-17 11:55:34 +0200

    [Misc] Remove testing stuffs from ND

--- plugins/sasl/data/modules/Custom Module/ND.lua
+++ plugins/sasl/data/modules/Custom Module/ND.lua
@@ -3,12 +3,10 @@ size = {900, 900}
 
 include('constants.lua')
 
-local B612MONO_regular = sasl.gl.loadFont("fonts/B612Mono-Regular.ttf")
-
 function draw()
 
-    sasl.gl.drawFrame(0, 0, 385, 380, {1,0,1})
-    sasl.gl.drawText(B612MONO_regular, 50, 50, "TEST", 10, false, false, TEXT_ALIGN_LEFT, ECAM_BLUE)
+    --sasl.gl.drawFrame(0, 0, 385, 380, {1,0,1})
+    --sasl.gl.drawText(B612MONO_regular, 50, 50, "TEST", 10, false, false, TEXT_ALIGN_LEFT, ECAM_BLUE)
 
     if get(AC_ess_shed_pwrd) == 0 then   -- TODO This should be fixed when screens move around
         return -- Bus is not powered on, this component cannot work
