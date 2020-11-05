6a57efe618467b8d94dcccc0ff81b4482c097dd3
JonathanOrr <61325694+JonathanOrr@users.noreply.github.com>
2020-11-06 08:21:41 +1100

    Update fctl

---
+++ plugins/sasl/data/modules/Custom Module/FBW_subcomponents/flight_ctl_subcomponents/vertical_ctl.lua
@@ -0,0 +1,52 @@
+function Elevator_control(vertical_input)
+    --HYD reversion
+    --left  G --> B
+    --right Y --> B
+    --flt computer reversion
+    --ELAC 2 --> ELAC 1 --> SEC 2 --> SEC 1
+    --flight computer THS motor relation
+    --MOTOR 1(ELAC 2)
+    --MOTOR 2(ELAC 1 | SEC 1)
+    --MOTOR 3(SEC 2)
+
+    local no_hyd_recenter_ias = 80
+    local elev_no_hyd_spd = 10
+    local elevators_speed = 45 --degrees per second
+
+    local max_up_deflection = -30
+    local max_dn_deflection = 17
+
+    --surface variables--
+    local l_elev_spd = 45
+    local r_elev_spd = 45
+
+    local l_elev_target = 0
+    local r_elev_target = 0
+
+    --SURFACE SPEEDS LOGIC--
+    --left  G --> B
+    --right Y --> B
+    l_elev_spd = Math_rescale(0, elev_no_hyd_spd, 1450, elevators_speed, get(Hydraulic_G_press) + get(Hydraulic_B_press))
+    r_elev_spd = Math_rescale(0, elev_no_hyd_spd, 1450, elevators_speed, get(Hydraulic_Y_press) + get(Hydraulic_B_press))
+
+    --TARGET DEFECTION LOGIC--
+    l_elev_target = Math_rescale(-1, max_dn_deflection, 0, 0, vertical_input) + Math_rescale(0, 0, 1, max_up_deflection, vertical_input)
+    r_elev_target = Math_rescale(-1, max_dn_deflection, 0, 0, vertical_input) + Math_rescale(0, 0, 1, max_up_deflection, vertical_input)
+
+    --if no elecrical control on both elevators then go to centering mode
+    --if no hydraulics control on both systems then go to damping mode
+    if get(ELAC_2_status) == 0 and get(ELAC_1_status) == 0 and get(SEC_2_status) == 0 and get(SEC_1_status) == 0 then
+        l_elev_target = 0
+        r_elev_target = 0
+    end
+
+    if get(Hydraulic_G_press) <= 1450 and get(Hydraulic_B_press) <= 1450 then
+        l_elev_target = Math_rescale(0, Math_rescale(0, max_dn_deflection, 1450, l_elev_target, (get(Hydraulic_G_press) + get(Hydraulic_B_press)) / 2), no_hyd_recenter_ias, 0, get(IAS))
+    end
+    if get(Hydraulic_Y_press) <= 1450 and get(Hydraulic_B_press) <= 1450 then
+        r_elev_target = Math_rescale(0, Math_rescale(0, max_dn_deflection, 1450, r_elev_target, (get(Hydraulic_Y_press) + get(Hydraulic_B_press)) / 2), no_hyd_recenter_ias, 0, get(IAS))
+    end
+
+    set(Elevators_hstab_1, Set_linear_anim_value(get(Elevators_hstab_1), l_elev_target, max_up_deflection, max_dn_deflection, l_elev_spd))
+    set(Elevators_hstab_2, Set_linear_anim_value(get(Elevators_hstab_2), r_elev_target, max_up_deflection, max_dn_deflection, r_elev_spd))
+end
--- plugins/sasl/data/modules/Custom Module/dynamic_datarefs.lua
+++ plugins/sasl/data/modules/Custom Module/dynamic_datarefs.lua
@@ -476,8 +476,8 @@ Flaps_in_transit = 	   createGlobalPropertyi("a321neo/dynamics/FBW/slats_and_fla
 Flaps_deployed_angle = createGlobalPropertyf("a321neo/dynamics/FBW/slats_and_flaps/flaps_deployed_angle", 0, false, true, false)--0, 0, 10, 15, 20, 40
 --stabilizers
 Horizontal_stabilizer_deflection = globalProperty("sim/flightmodel2/controls/stabilizer_deflection_degrees")
-Elevators_hstab_1 = globalProperty("sim/flightmodel/controls/hstab1_elv1def") --elevators -17 deg down 30 deg up
-Elevators_hstab_2 = globalProperty("sim/flightmodel/controls/hstab2_elv1def") --elevators -17 deg down 30 deg up
+Elevators_hstab_1 = globalProperty("sim/flightmodel/controls/hstab1_elv1def") --elevators 17 deg down -30 deg up
+Elevators_hstab_2 = globalProperty("sim/flightmodel/controls/hstab2_elv1def") --elevators 17 deg down -30 deg up
 Rudder = globalProperty("sim/flightmodel/controls/vstab1_rud1def") --rudder 30 deg left -30 deg right
 Yaw_lim = createGlobalPropertyf("a321neo/dynamics/FBW/yaw_lim", 30, false, true, false)
 
