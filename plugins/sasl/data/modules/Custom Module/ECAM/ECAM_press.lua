8665606de7789659769b0fb4a85c4fd2b61524c1
Rico Rico <rico23rico@users.noreply.github.com>
2021-01-12 01:19:36 +0100

    [Fire] Fix behavior of cargo aft isol and extractor when cargo smoke detect

--- plugins/sasl/data/modules/Custom Module/ECAM/ECAM_press.lua
+++ plugins/sasl/data/modules/Custom Module/ECAM/ECAM_press.lua
@@ -101,13 +101,13 @@ local function draw_valves_text()
     sasl.gl.drawText(Font_AirbusDUL, size[1]/2-205, 270, "INLET", 34, false, false, TEXT_ALIGN_CENTER, get(FAILURE_AVIONICS_INLET) == 1 and ECAM_ORANGE or ECAM_WHITE)
     sasl.gl.drawText(Font_AirbusDUL, size[1]/2-10, 270, "OUTLET", 34, false, false, TEXT_ALIGN_CENTER, get(FAILURE_AVIONICS_OUTLET) == 1 and ECAM_ORANGE or ECAM_WHITE)
 
-    local faulty_blower_or_extract = get(FAILURE_AIRCOND_VENT_BLOWER) == 1 or get(FAILURE_AIRCOND_VENT_EXTRACT) == 1
+    local faulty_blower_or_extract = get(FAILURE_AIRCOND_VENT_BLOWER) == 1 or get(FAILURE_AIRCOND_VENT_EXTRACT) == 1 or get(Fire_cargo_fwd_smoke_detected) == 1
     sasl.gl.drawText(Font_AirbusDUL, size[1]/2-115, 330, "VENT", 34, false, false, TEXT_ALIGN_CENTER, faulty_blower_or_extract and ECAM_ORANGE or ECAM_WHITE)
 
     if get(FAILURE_AIRCOND_VENT_BLOWER) == 1 then
         sasl.gl.drawText(Font_AirbusDUL, size[1]/2-240, 330, "BLOWER", 34, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
     end
-    if get(FAILURE_AIRCOND_VENT_EXTRACT) == 1 then
+    if get(FAILURE_AIRCOND_VENT_EXTRACT) == 1 or get(Fire_cargo_fwd_smoke_detected) == 1 then
         sasl.gl.drawText(Font_AirbusDUL, size[1]/2+20, 330, "EXTRACT", 34, false, false, TEXT_ALIGN_CENTER, ECAM_ORANGE)
     end
 end
