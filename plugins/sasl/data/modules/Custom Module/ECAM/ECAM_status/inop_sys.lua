20288bf522a89663a51bb41fcdce76846dae1545
Rico Rico <rico23rico@users.noreply.github.com>
2021-04-24 13:23:15 +0200

    [ECAM/Status] INOP SYS -> anti-ice/elac/sec/fac

--- plugins/sasl/data/modules/Custom Module/ECAM/ECAM_status/inop_sys.lua
+++ plugins/sasl/data/modules/Custom Module/ECAM/ECAM_status/inop_sys.lua
@@ -1,148 +1,142 @@
 
-local function put_inop_sys_msg_2(messages, dr_1, dr_2, title)
-    if get(dr_1) == 0 and get(dr_2) == 0 then
-        table.insert(messages, title .. " 1 + 2")
-    elseif get(dr_1) == 0 then
-        table.insert(messages, title .. " 1")
-    elseif get(dr_2) == 0 then
-        table.insert(messages, title .. " 2")
+local function put_inop_sys_msg_2(messages, dr_1, dr_2, t_1, t_2, title)
+    if dr_1 and dr_2 then
+        return ( title .. " " .. t_1 .. " + " .. t_2)
+    elseif dr_1 then
+        return ( title .. " " .. t_1)
+    elseif dr_2 then
+        return ( title .. " " .. t_2)
     end
+    return "XXX"    -- Shold not happen
 end
 
 local function put_inop_sys_msg_3(messages, dr_1, dr_2, dr_3, title)
-    if get(dr_1) == 0 and get(dr_2) == 0 and get(dr_3) == 0 then
-        table.insert(messages, title .. " 1 + 2 + 3")
-    elseif get(dr_1) == 0 and get(dr_2) == 0 then
-        table.insert(messages, title .. " 1 + 2")
-    elseif get(dr_1) == 0 and get(dr_3) == 0 then
-        table.insert(messages, title .. " 1 + 3")
-    elseif get(dr_2) == 0 and get(dr_3) == 0 then
-        table.insert(messages, title .. " 2 + 3")
-    elseif get(dr_1) == 0 then
-        table.insert(messages, title .. " 1")
-    elseif get(dr_2) == 0 then
-        table.insert(messages, title .. " 2")
-    elseif get(dr_3) == 0 then
-        table.insert(messages, title .. " 3")
+    if dr_1 and dr_2 and dr_3 then
+        return ( title .. " 1 + 2 + 3")
+    elseif dr_1 == 0 and dr_2 == 0 then
+        return ( title .. " 1 + 2")
+    elseif dr_1 == 0 and dr_3 == 0 then
+        return ( title .. " 1 + 3")
+    elseif dr_2 == 0 and dr_3 == 0 then
+        return ( title .. " 2 + 3")
+    elseif dr_1 == 0 then
+        return ( title .. " 1")
+    elseif dr_2 == 0 then
+        return ( title .. " 2")
+    elseif dr_3 == 0 then
+        return ( title .. " 3")
     end
+    return "XXX"    -- Shold not happen
 end
 
+local inop_systems_desc = {
 
+    -- ANTI-ICE
+    {
+     text = "CAPT PITOT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_PITOT_CAPT) == 1 or get(AC_ess_bus_pwrd) == 0 end,
+    },
+    {
+     text = "F/O PITOT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_PITOT_FO) == 1 or get(AC_bus_2_pwrd) == 0 end,
+    },
+    {
+     text = "STBY PITOT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_PITOT_STDBY) == 1 or get(AC_bus_1_pwrd) == 0 end,
+    },
+    {
+     text = "CAPT AOA", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_AOA_CAPT) == 1 or get(AC_ess_shed_pwrd) == 0 end,
+    },
+    {
+     text = "F/O AOA", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_AOA_FO) == 1 or get(AC_bus_2_pwrd) == 0 end,
+    },
+    {
+     text = "STBY AOA", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_AOA_STDBY) == 1 or get(AC_bus_1_pwrd) == 0 end,
+    },
+    {
+     text = "CAPT STAT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_SP_CAPT) == 1 or get(DC_bus_1_pwrd) == 0 end,
+    },
+    {
+     text = "F/O STAT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_SP_FO) == 1 or get(DC_bus_2_pwrd) == 0 end,
+    },
+    {
+     text = "STBY STAT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_SP_STDBY) == 1 or get(DC_bus_1_pwrd) == 0 end,
+    },
+    {
+     text = "CAPT TAT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_TAT_CAPT) == 1 or get(AC_bus_1_pwrd) == 0 end,
+    },
+    {
+     text = "F/O TAT", nr = 1,
+     cond_1 = function() return get(FAILURE_AI_TAT_FO) == 1 or get(AC_bus_2_pwrd) == 0 end,
+    },
+    {
+     text = "ENG", text_after = "A.ICE", nr = 2,
+     cond_1 = function() return not AI_sys.comp[ANTIICE_ENG_1].valve_status and (get(DC_bus_1_pwrd) == 0 or get(FAILURE_AI_Eng1_valve_stuck) == 1) end,
+     cond_2 = function() return not AI_sys.comp[ANTIICE_ENG_2].valve_status and (get(DC_bus_2_pwrd) == 0 or get(FAILURE_AI_Eng2_valve_stuck) == 1) end,
+    },
+    {
+     text = "WSHLD", text_after = "HEAT", text_1="L", text_2="R", nr = 2,
+     cond_1 = function() return get(AI_sys.comp[ANTIICE_WINDOW_HEAT_L].failure) == 1 or get(AI_sys.comp[ANTIICE_WINDOW_HEAT_L].source_elec) == 0 end,
+     cond_2 = function() return get(AI_sys.comp[ANTIICE_WINDOW_HEAT_R].failure) == 1 or get(AI_sys.comp[ANTIICE_WINDOW_HEAT_R].source_elec) == 0 end,
+    },
+    
+    -- F/CTL
+    {
+     text = "ELAC",
+     nr = 2,
+     cond_1 = function() return get(FAILURE_FCTL_ELAC_1) == 1 or (get(DC_ess_bus_pwrd) == 0 and get(HOT_bus_1_pwrd) == 0) end,
+     cond_2 = function() return get(FAILURE_FCTL_ELAC_2) == 1 or (get(DC_bus_2_pwrd) == 0 and get(HOT_bus_2_pwrd) == 0) end,
+    },
+    {
+     text = "SEC",
+     nr = 3,
+     cond_1 = function() return get(FAILURE_FCTL_SEC_1) == 1 or (get(DC_ess_bus_pwrd) == 0 and get(HOT_bus_1_pwrd) == 0) end,
+     cond_2 = function() return get(FAILURE_FCTL_SEC_2) == 1 or (get(DC_bus_2_pwrd) == 0) end,
+     cond_3 = function() return get(FAILURE_FCTL_SEC_3) == 1 or (get(DC_bus_2_pwrd) == 0) end,
+    },
+    {
+     text = "FAC",
+     nr = 2,
+     cond_1 = function() return get(FAILURE_FCTL_FAC_1) == 1 or not (get(AC_ess_bus_pwrd) == 1 and get(DC_shed_ess_pwrd) == 1) end,
+     cond_2 = function() return get(FAILURE_FCTL_FAC_2) == 1 or not (get(AC_bus_2_pwrd) == 1 and get(DC_bus_2_pwrd) == 1) end,
+    },
+
+}
 
 
 function ECAM_status_get_inop_sys()
 
         local messages = {}
 
-        local inop_cat3_dual = false
-        local inop_steer = false
-        local inop_aps   = false
-        local inop_atr   = false
-
-        -- AIR
-        --put_inop_sys_msg_2(messages, Left_bleed_avil, Right_bleed_avil, "PACK")
-
-        -- ELAC / SEC / FAC
-        put_inop_sys_msg_2(messages, ELAC_1_status, ELAC_2_status, "ELAC")
-        put_inop_sys_msg_3(messages, SEC_1_status, SEC_2_status, SEC_3_status, "SEC")
-        put_inop_sys_msg_2(messages, FAC_1_status, FAC_2_status, "FAC")
-
         -- FBW
         if get(FBW_total_control_law) < FBW_NORMAL_LAW then
             table.insert(messages, "F/CTL PROT")
         end
 
-        -- ELEC
-        --put_inop_sys_msg_2(messages, Gen_1_on, Gen_2_on, "GEN")
-
-        -- ENGINES and APU
-        --if get(FAILURE_Apu) == 6 or get(FAILURE_Apu_fire) == 6 then
-        --    table.insert(messages, "APU")
-        --end
-
-        -- L/G
-        --if get(FAILURE_gear) == 1 then
-        --    table.insert(messages, "L/G RETRACT")
-        --elseif get(FAILURE_gear) == 2 then
-        --    inop_steer = true
-        --    inop_cat3_dual = true
-        --end
-
-
-        --if get(FAILURE_TCAS) == 6 then
-        --    table.insert(messages, "TCAS")                
-        --end
-
-        --if get(Nosewheel_Steering_working) then
-        --    inop_steer = true
-        --end
-
-
---        if get(Nosewheel_Steering_and_AS) == 0 then
---            table.insert(messages, "ANTI SKID")
---            table.insert(messages, "BSCU CH 1")
---            table.insert(messages, "BSCU CH 2")
---            inop_cat3_dual = true            
---        end
-
-        --[[if MessageGroup_ADR_FAULT_SINGLE:is_active() then
-            table.insert(messages, "ADR " .. MessageGroup_ADR_FAULT_SINGLE:get_failed())
-            inop_cat3_dual = true
-        end
-        if MessageGroup_ADR_FAULT_DOUBLE:is_active() then
-            a, b = MessageGroup_ADR_FAULT_DOUBLE:get_failed()
-            table.insert(messages, "ADR " .. a .. " + " .. b)
-            table.insert(messages, "RUD TRV LIM")
-            inop_cat3_dual = true
-            inop_aps = true
-        end
-        
-        --if MessageGroup_ADR_FAULT_TRIPLE:is_active() then
-        --    table.insert(messages, "ADR 1 + 2 + 3")
-        --    table.insert(messages, "RUD TRV LIM")
-        --    table.insert(messages, "WINDSHEAR DET")
-        --    inop_cat3_dual = true
-        --    inop_aps = true
-        --    inop_atr = true
-        --end
- 
-        if MessageGroup_IR_FAULT_SINGLE:is_active() then
-            table.insert(messages, "IR " .. MessageGroup_IR_FAULT_SINGLE:get_failed())
-            inop_cat3_dual = true
-        end
-        if MessageGroup_IR_FAULT_DOUBLE:is_active() then
-            a,b = MessageGroup_IR_FAULT_DOUBLE:get_failed()
-            table.insert(messages, "IR " .. a .. " + " .. b)
-            table.insert(messages, "YAW DAMPER")
-            inop_cat3_dual = true
-            inop_aps = true
-            inop_atr = true
-        end
-        
-        --if MessageGroup_IR_FAULT_TRIPLE:is_active() then
-        --    table.insert(messages, "IR 1 + 2 + 3")
-        --    table.insert(messages, "YAW DAMPER")
-        --    inop_cat3_dual = true
-        --    inop_aps = true
-        --    inop_atr = true
-        --end
-        ]]--
-        -- LEAVE THESE AT THE LAST
-        if inop_cat3_dual then
-            table.insert(messages, "CAT 3 DUAL")        
+        for l,x in pairs(inop_systems_desc) do
+            if x.nr == 1 and x.cond_1() then
+                table.insert(messages, x.text)
+            elseif x.nr == 2 and (x.cond_1() or x.cond_2()) then
+                local t1 = x.text_1 and x.text_1 or "1"
+                local t2 = x.text_2 and x.text_2 or "2"
+                local msg = put_inop_sys_msg_2(messages, x.cond_1(), x.cond_2(), t1, t2, x.text)
+                if x.text_after then msg = msg .. " " .. x.text_after end
+                table.insert(messages, msg)
+            elseif x.nr == 3 and (x.cond_1() or x.cond_2() or x.cond_3()) then
+                local msg = put_inop_sys_msg_3(messages, x.cond_1(), x.cond_2(), x.cond_3(), x.text)
+                if x.text_after then msg = msg .. " " .. x.text_after end
+                table.insert(messages, msg)
+            end
         end
 
-        if inop_aps then
-            table.insert(messages, "AP 1 + 2")        
-        end
-        
-        if inop_atr then
-            table.insert(messages, "A/THR")        
-        end
-                
-        if inop_steer then
-            table.insert(messages, "N.W. STEER")
-        end
+
 
         return messages
     end
