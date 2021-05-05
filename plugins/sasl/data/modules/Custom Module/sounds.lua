ab93ccf32293a3c220a229c2fa331eb458d521c7
Rico Rico <rico23rico@users.noreply.github.com>
2021-05-05 09:44:29 +0200

    [Sounds] New blower logic

--- plugins/sasl/data/modules/Custom Module/sounds.lua
+++ plugins/sasl/data/modules/Custom Module/sounds.lua
@@ -19,17 +19,27 @@
 include('sounds_GPWS.lua')
 
 -------------------------------------------------------------------------------
+-- Constants
+-------------------------------------------------------------------------------
+local BLOWER_VOL_RISE_TIME    = 1       -- Time (in sec) required to go from 0 to max volume (self-test)
+local BLOWER_SHUTDOWN_DELAY   = 0.5     -- This must be higher than the elec bus switch time
+local BLOWER_START_DELAY_TIME = 5
+local BLOWER_SELF_TEST_TIME   = 8
+
+-------------------------------------------------------------------------------
 -- Datarefs
 -------------------------------------------------------------------------------
 
-Sounds_elec_bus_delayed = createGlobalPropertyf("a321neo/sounds/elec_bus_delayed", 0, false, true, false)
-Sounds_blower_delayed   = createGlobalPropertyf("a321neo/sounds/blower_delayed", 0, false, true, false)
-Sounds_extract_delayed  = createGlobalPropertyf("a321neo/sounds/extract_delayed", 0, false, true, false)
+local Sounds_elec_bus_delayed = createGlobalPropertyf("a321neo/sounds/elec_bus_delayed", 0, false, true, false)
+local Sounds_blower_volume    = createGlobalPropertyf("a321neo/sounds/blower_volume", 0, false, true, false)    -- 0: OFF, 0.5: NORMAL, 1: TEST
+local Sounds_extract_delayed  = createGlobalPropertyf("a321neo/sounds/extract_delayed", 0, false, true, false)
 
 -------------------------------------------------------------------------------
 -- Global variables
 -------------------------------------------------------------------------------
+local blower_stop_time  = 0
 local blower_start_time = 0
+local blower_fast_start = false
 
 -------------------------------------------------------------------------------
 -- Functions
@@ -73,22 +83,65 @@ local function elec_delays()
 
 end
 
-local function blower_extract_delay()
+local function update_blower()
 
-    if get(AC_bus_1_pwrd) == 1 and get(FAILURE_AIRCOND_VENT_BLOWER) == 0 and blower_start_time == 0 then
+    -- Blower logic:
+    -- 1. It delays for 5 second on startup +8 seconds for test
+    -- 2. it turns on and off instantly on override switch
+    -- 3. It does not restart in a power swap
+
+    if get(FAILURE_AIRCOND_VENT_BLOWER) == 1 or get(Ventilation_blower_override) == 1 then
+        -- No chance here, the blower is failed or the override switch pressed
+        -- switch off immediately
+        Set_dataref_linear_anim(Sounds_blower_volume, 0, 0, 1, BLOWER_VOL_RISE_TIME)
+        blower_fast_start = true    -- In this case, the next start would be without the delay and
+                                    -- self test
+        return
+    end
+
+    if blower_fast_start and get(Ventilation_blower_running) == 1 then
+        blower_stop_time = 0
+        Set_dataref_linear_anim(Sounds_blower_volume, 0.5, 0, 1, BLOWER_VOL_RISE_TIME)
+        return
+    end
+
+    blower_fast_start = false
+
+    if get(Ventilation_blower_running) == 0 then 
+        if blower_stop_time == 0 then
+            -- This happens only when the elec is off
+            blower_stop_time = get(TIME)
+        end
+
+        if get(TIME) - blower_stop_time > BLOWER_SHUTDOWN_DELAY then
+            Set_dataref_linear_anim(Sounds_blower_volume, 0, 0, 1, BLOWER_VOL_RISE_TIME)
+            blower_start_time = 0
+            return
+        end
+
+        return -- Otherwise wait and stay with the previous setting
+    end
+
+    -- Ok if we are here, the blower is running (or about to run)
+    blower_stop_time = 0
+
+    if blower_start_time == 0 then
         blower_start_time = get(TIME)
-    elseif not(get(AC_bus_1_pwrd) == 1 and get(FAILURE_AIRCOND_VENT_BLOWER) == 0) then
-        blower_start_time = 0
     end
 
-    if get(Ventilation_blower_running) == 1 and  get(TIME) - blower_start_time < 5 then
-        set(Sounds_blower_delayed, 0)
-    elseif get(Ventilation_blower_running) == 1 and  get(TIME) - blower_start_time > 5 then
-        set(Sounds_blower_delayed, 1)
-    else
-        set(Sounds_blower_delayed, 0)
+    local diff_time = get(TIME) - blower_start_time
+    if diff_time > BLOWER_START_DELAY_TIME + BLOWER_SELF_TEST_TIME or get(All_on_ground) == 0 then
+        -- Normal sound
+        Set_dataref_linear_anim(Sounds_blower_volume, 0.5, 0, 1, BLOWER_VOL_RISE_TIME)
+    elseif diff_time > BLOWER_START_DELAY_TIME then
+        -- Self-test sound
+        Set_dataref_linear_anim(Sounds_blower_volume, 1, 0, 1, BLOWER_VOL_RISE_TIME)
     end
-    
+
+end
+
+local function update_extract()
+
     Set_dataref_linear_anim(Sounds_extract_delayed, get(Ventilation_extract_running), 0, 1, 0.2)
 
 end
@@ -109,7 +162,8 @@ end
 
 function update()
     elec_delays()
-    blower_extract_delay()
+    update_blower()
+    update_extract()
     gpws_sounds()
     thrust_rush()
     reverser_drfs()
