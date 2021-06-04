f00e60d43335721fc32e739b707a0d38c57e62e4
jamlen <code@james-allen.co.uk>
2021-06-04 10:57:34 +0100

    Getting the Align TTN

--- plugins/sasl/data/modules/Custom Module/MCDU/pages/500-DATA/503-irs-monitor.lua
+++ plugins/sasl/data/modules/Custom Module/MCDU/pages/500-DATA/503-irs-monitor.lua
@@ -16,11 +16,20 @@
 
 local THIS_PAGE = MCDU_Page:new({id=503})
 
+function getAlignTTN(i)
+  return math.max(0, IR_TIME_TO_GET_ATTITUDE - (get(TIME) - ADIRS_sys[i].ir_align_start_time))
+end
+
 function THIS_PAGE:render(mcdu_data)
     self:set_title(mcdu_data, "IRS MONITOR")
 
-    
-    self:set_line(mcdu_data, MCDU_LEFT, 4, "PAGE NOT YET IMPLEMENTED", MCDU_LARGE, ECAM_MAGENTA)
+    -- self:set_line(mcdu_data, MCDU_LEFT, 4, "PAGE NOT YET IMPLEMENTED", MCDU_LARGE, ECAM_MAGENTA)
+    self:set_line(mcdu_data, MCDU_LEFT, 1, "<IRS1", MCDU_LARGE, ECAM_WHITE)
+    if ADIRS_sys[ADIRS_1].ir_status == IR_STATUS_IN_ALIGN then
+      self:set_line(mcdu_data, MCDU_LEFT, 2, " ALIGN TTN " .. getAlignTTN(ADIRS_1), MCDU_SMALL, ECAM_WHITE)
+    elseif ADIRS_sys[ADIRS_1].ir_status == IR_STATUS_ALIGNED then
+      self:set_line(mcdu_data, MCDU_LEFT, 2, " NAV", MCDU_SMALL, ECAM_GREEN)
+    end
 end
 
 
