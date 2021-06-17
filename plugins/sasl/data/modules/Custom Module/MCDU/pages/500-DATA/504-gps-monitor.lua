68af2781605358b6bb69dbc4331dad568c9f853a
jamlen <code@james-allen.co.uk>
2021-06-17 23:28:59 +0100

    GPS Monitor page. Some placeholder values for now

--- plugins/sasl/data/modules/Custom Module/MCDU/pages/500-DATA/504-gps-monitor.lua
+++ plugins/sasl/data/modules/Custom Module/MCDU/pages/500-DATA/504-gps-monitor.lua
@@ -16,11 +16,72 @@
 
 local THIS_PAGE = MCDU_Page:new({id=504})
 
+local function fz(x)
+  if x < 10 then
+    return "0" .. x
+  else
+    return x
+  end
+end
+
+local function get_gps_time()
+  if get(GPS_1_is_available) == 1 or get(GPS_2_is_available) == 1 then
+    return fz(get(ZULU_hours)) .. mcdu_format_force_to_small(":") .. fz(get(ZULU_mins)) .. mcdu_format_force_to_small(":") .. fz(get(ZULU_secs))
+  else
+    -- No GPS info? No party
+    return "------"
+  end
+end
+
 function THIS_PAGE:render(mcdu_data)
     self:set_title(mcdu_data, "GPS MONITOR")
 
-    
-    self:set_line(mcdu_data, MCDU_LEFT, 4, "PAGE NOT YET IMPLEMENTED", MCDU_LARGE, ECAM_MAGENTA)
+    -- self:set_line(mcdu_data, MCDU_LEFT, 4, "PAGE NOT YET IMPLEMENTED", MCDU_LARGE, ECAM_MAGENTA)
+
+    self:set_line(mcdu_data, MCDU_LEFT, 1, "GPS1 POSITION", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_LEFT, 1, mcdu_lat_lon_to_str(get(GPS_1_lat), get(GPS_1_lon)), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_LEFT, 2, "TTRK", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_LEFT, 2, mcdu_pad_dp(ADIRS_sys[ADIRS_1].track, 1), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_LEFT, 3, "MERIT", MCDU_SMALL, ECAM_WHITE)
+    -- TODO have an option/config for meters, then pad with space
+    self:set_line(mcdu_data, MCDU_LEFT, 3, "300FT",  MCDU_LARGE, ECAM_GREEN)
+
+    self:set_line(mcdu_data, MCDU_CENTER, 2, "UTC", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_CENTER, 2, get_gps_time(), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_CENTER, 3, "GPS ALT", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_CENTER, 3, mcdu_pad_dp(get(GPS_1_altitude), 1), MCDU_LARGE, ECAM_GREEN)
+
+    self:set_line(mcdu_data, MCDU_RIGHT, 2, "GS", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_RIGHT, 2, math.floor(ADIRS_sys[ADIRS_1].gs), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_RIGHT, 3, "MODE/SAT", MCDU_SMALL, ECAM_WHITE)
+    if get(GPS_1_is_available) == 1 then
+      self:set_line(mcdu_data, MCDU_RIGHT, 3, "NAV/6", MCDU_LARGE, ECAM_GREEN)
+    else
+      self:set_line(mcdu_data, MCDU_RIGHT, 3, "ACQ", MCDU_LARGE, ECAM_GREEN)
+    end
+
+
+    self:set_line(mcdu_data, MCDU_LEFT, 4, "GPS2 POSITION", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_LEFT, 4, mcdu_lat_lon_to_str(get(GPS_2_lat), get(GPS_2_lon)), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_LEFT, 5, "TTRK", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_LEFT, 5, mcdu_pad_dp(ADIRS_sys[ADIRS_2].track, 1), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_LEFT, 6, "MERIT", MCDU_SMALL, ECAM_WHITE)
+    -- TODO have an option/config for meters, then pad with space
+    self:set_line(mcdu_data, MCDU_LEFT, 6, "300FT",  MCDU_LARGE, ECAM_GREEN)
+
+    self:set_line(mcdu_data, MCDU_CENTER, 5, "UTC", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_CENTER, 5, get_gps_time(), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_CENTER, 6, "GPS ALT", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_CENTER, 6, mcdu_pad_dp(get(GPS_2_altitude), 1), MCDU_LARGE, ECAM_GREEN)
+
+    self:set_line(mcdu_data, MCDU_RIGHT, 5, "GS", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_RIGHT, 5, math.floor(ADIRS_sys[ADIRS_2].gs), MCDU_LARGE, ECAM_GREEN)
+    self:set_line(mcdu_data, MCDU_RIGHT, 6, "MODE/SAT", MCDU_SMALL, ECAM_WHITE)
+    if get(GPS_2_is_available) == 1 then
+      self:set_line(mcdu_data, MCDU_RIGHT, 6, "NAV/6", MCDU_LARGE, ECAM_GREEN)
+    else
+      self:set_line(mcdu_data, MCDU_RIGHT, 6, "ACQ", MCDU_LARGE, ECAM_GREEN)
+    end
 end
 
 
