23ca7814b93ae334b702eda8c66edc195be2931f
Rico Rico <rico23rico@users.noreply.github.com>
2021-08-23 22:27:08 +0200

    [MCDU] Insert and matching airways in 611

--- plugins/sasl/data/modules/Custom Module/MCDU/pages/600-FPLN/611-airways.lua
+++ plugins/sasl/data/modules/Custom Module/MCDU/pages/600-FPLN/611-airways.lua
@@ -22,7 +22,15 @@ function THIS_PAGE:render_awys(mcdu_data)
     local end_i   = math.min(#mcdu_data.page_data[602].awys, start_i + 5) -- Current airways
     
     for i=start_i, end_i do
-        self:set_line(mcdu_data, MCDU_LEFT, curr_displayed_line, ""..i, MCDU_SMALL, ECAM_WHITE)
+        self:set_line(mcdu_data, MCDU_LEFT, curr_displayed_line, " VIA", MCDU_SMALL, ECAM_WHITE)
+        self:set_line(mcdu_data, MCDU_LEFT, curr_displayed_line, mcdu_data.page_data[602].awys[i].id, MCDU_LARGE, ECAM_BLUE)
+
+        self:set_line(mcdu_data, MCDU_RIGHT, curr_displayed_line, "TO ", MCDU_SMALL, ECAM_WHITE)
+        if i < end_i then
+            self:set_line(mcdu_data, MCDU_RIGHT, curr_displayed_line, mcdu_data.page_data[602].awys[i+1].begin_point, MCDU_LARGE, ECAM_BLUE)
+        else
+            self:set_line(mcdu_data, MCDU_RIGHT, curr_displayed_line, "[     ]", MCDU_LARGE, ECAM_BLUE)    
+        end
 
         curr_displayed_line = curr_displayed_line + 1
         if curr_displayed_line >= 6 then
@@ -35,7 +43,7 @@ function THIS_PAGE:render_awys(mcdu_data)
     end
 
     if curr_displayed_line < 6 then
-        self:set_line(mcdu_data, MCDU_LEFT, curr_displayed_line, "VIA", MCDU_SMALL, ECAM_WHITE)
+        self:set_line(mcdu_data, MCDU_LEFT, curr_displayed_line, " VIA", MCDU_SMALL, ECAM_WHITE)
         self:set_line(mcdu_data, MCDU_LEFT, curr_displayed_line, "[   ]", MCDU_LARGE, ECAM_BLUE)
     end
 
@@ -54,7 +62,7 @@ function THIS_PAGE:render(mcdu_data)
     local source_name = mcdu_data.airways.source_wpt.id
 
     if not mcdu_data.page_data[602] then
-        mcdu_data.page_data[602] = { awys={{},{},{},{},{},{}}, curr_page=0 }
+        mcdu_data.page_data[602] = { awys={}, curr_page=0 }
     end
 
     local main_col = FMGS_does_temp_fpln_exist() and ECAM_YELLOW or ECAM_GREEN
@@ -75,9 +83,85 @@ function THIS_PAGE:render(mcdu_data)
 
 end
 
-function THIS_PAGE:L1(mcdu_data)
-    MCDU_Page:L1(mcdu_data) -- Error
+function THIS_PAGE:add_airway(mcdu_data, id)
+    id = id + mcdu_data.page_data[602].curr_page * 5
+    if id > #mcdu_data.page_data[602].awys + 1 then
+        MCDU_Page:L1(mcdu_data)
+        return
+    end
+
+    if not(AvionicsBay.is_initialized() and AvionicsBay.is_ready()) then
+        mcdu_send_message(mcdu_data, "AVIONICSBAY NOT READY")
+        return
+    end
+
+
+    local input = mcdu_get_entry(mcdu_data)
+    local awy_points
+    if #input > 0 and #input < 6 then
+        awy_points = AvionicsBay.awys.get_by_id(input)
+    else
+        mcdu_send_message(mcdu_data, "FORMAT ERROR")
+        return
+    end
+
+    if #awy_points == 0 then
+        mcdu_send_message(mcdu_data, "NOT IN DATABASE")
+        return
+    end
+
+    -- Check if at least one point is the previous (or the LAT REV one)
+    local intersection_point
+    if id == 1 then
+        for _,k in ipairs(awy_points) do
+            if k.start_wpt == mcdu_data.airways.source_wpt.id then
+                intersection_point = k.start_wpt
+                break
+            end
+        end
+    else
+        -- TODO find intersection point
+        local prev_points = mcdu_data.page_data[602].awys[id-1].all_points
+        for _,a in ipairs(prev_points) do
+            for _,b in ipairs(awy_points) do
+                if a.end_wpt == b.start_wpt then
+                    intersection_point = b.start_wpt
+                    break
+                end
+            end
+        end
+    end
+
+    if not intersection_point then
+        mcdu_send_message(mcdu_data, "NO INTERSECTION FOUND")
+        return
+    else
+        table.insert(mcdu_data.page_data[602].awys, {id=input, begin_point=intersection_point, all_points=awy_points})
+    end
+
 end
+
+
+function THIS_PAGE:L1(mcdu_data)
+    self:add_airway(mcdu_data, 1)
+end
+
+function THIS_PAGE:L2(mcdu_data)
+    self:add_airway(mcdu_data, 2)
+end
+
+function THIS_PAGE:L3(mcdu_data)
+    self:add_airway(mcdu_data, 3)
+end
+
+function THIS_PAGE:L4(mcdu_data)
+    self:add_airway(mcdu_data, 4)
+end
+
+function THIS_PAGE:L5(mcdu_data)
+    self:add_airway(mcdu_data, 5)
+end
+
 
 function THIS_PAGE:Slew_Down(mcdu_data)
     if mcdu_data.page_data[602].curr_page == 0 then
