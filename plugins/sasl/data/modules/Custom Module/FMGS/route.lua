a926cc60ab24d700ad2f342395f466297cdb2e45
Rico Rico <rico23rico@users.noreply.github.com>
2021-11-07 17:11:43 +0100

    [FMGS] Cleanup old code

--- plugins/sasl/data/modules/Custom Module/FMGS/route.lua
+++ plugins/sasl/data/modules/Custom Module/FMGS/route.lua
@@ -20,48 +20,9 @@ include('FMGS/functions.lua')
 include('FMGS/nav_helpers.lua')
 include('FMGS/geometric_helpers.lua')
 include('FMGS/cifp_decorator.lua')
+include('FMGS/path_generation/cifp_to_segment.lua')
+include('FMGS/path_generation/turn_computer.lua')
 include('libs/geo-helpers.lua')
-local ROUTE_FREQ_UPDATE_SEC = 0.5
-
-local route_last_update = 0
-
-local function compute_arc_radius(tas, roll_limit)
-    return math.max(0, math.floor(tas)^2 / (math.tan(math.rad(roll_limit)) * 11.294) * 0.000164579)
-end
-
-local function update_active_fpln()
-    local fpln_active = FMGS_sys.fpln.active.legs
-    
-    local nr_points = #fpln_active
-    if nr_points <= 1 then
-        return  -- We need at least 2 waypoints to build a route...
-    end
-    
-    local roll_limit = FMGS_get_roll_limit()
-    local NM_ARC = compute_arc_radius(adirs_get_avg_tas(), roll_limit)
-    
-    for k,r in ipairs(fpln_active) do
-
-        if k > 1 and k < nr_points then
-            local in_between_discontinuity = r.discontinuity or fpln_active[k-1].discontinuity or fpln_active[k+1].discontinuity
-            if not in_between_discontinuity then
-                -- Limited to 0.5 to avoid that when the aircraft is too fast a too large curve would overshoot the next curve.
-                lat_start, lon_start = point_from_a_segment_lat_lon_limited(r.lat, r.lon, fpln_active[k-1].lat, fpln_active[k-1].lon, NM_ARC, 0.5)
-                lat_end,   lon_end   = point_from_a_segment_lat_lon_limited(r.lat, r.lon, fpln_active[k+1].lat, fpln_active[k+1].lon, NM_ARC, 0.5)
-                
-                fpln_active[k].beizer = {
-                    start_lat =  lat_start,
-                    start_lon =  lon_start,
-                    end_lat   =  lat_end,
-                    end_lon   =  lon_end,
-                }
-            else
-                 -- TODO manage discontinuity
-            end
-        end
-    end
-    
-end
 
 local function update_cifp(apt_ref, reference, initial_point)
     if not reference then
@@ -100,37 +61,6 @@ local function update_cifp(apt_ref, reference, initial_point)
     return total_distance, prev_point
 end
 
-local function debug_add_route()
-
-    -- Disable for testing
-    if get(TIME) - route_last_update < ROUTE_FREQ_UPDATE_SEC then
-        return
-    end
-    
-    if AvionicsBay.is_initialized() and AvionicsBay.is_ready() then
-        if not FMGS_sys.fpln.active.apts.dep_sid then
-            if not FMGS_sys.fpln.temp then
-                FMGS_set_apt_dep("LIML")
-                FMGS_set_apt_arr("LIMC")
-                FMGS_set_apt_alt("VABB")
-                FMGS_create_temp_fpln()
-                FMGS_dep_set_rwy(FMGS_sys.fpln.temp.apts.dep.rwys[1], true)
-                logInfo("DEBUG F/PLN is active: LOADED 1/2")
-            end
-            if FMGS_sys.fpln.temp.apts.dep_cifp then
-                FMGS_dep_set_sid(FMGS_sys.fpln.temp.apts.dep_cifp.sids[49])
-                FMGS_dep_set_trans(FMGS_sys.fpln.temp.apts.dep_cifp.sids[50])
-                FMGS_reshape_fpln()
-                FMGS_insert_temp_fpln()
-                logInfo("DEBUG F/PLN is active: LOADED 2/2")
-                route_last_update = get(TIME) + 100000000
-            end
-        end
-    end
-   
-    route_last_update = get(TIME)
-
-end
 
 local function fpln_recompute_distances_fplnlegs(fpln, prev_point)
 
@@ -159,8 +89,7 @@ end
 
 function update_route()
 
-    --debug_add_route()
-
+    -- First of all let's understand which flight plan needs recomputing (precedence given to active F/PLN)
     local fpln
     if FMGS_sys.fpln.active.require_recompute then
         fpln = FMGS_sys.fpln.active
@@ -170,6 +99,7 @@ function update_route()
     end
 
     if not fpln then
+        -- No F/PLN requires recomputing, bye bye
         return
     end
 
