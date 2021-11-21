be76f8d7bdbd5bb2b177219f1a0f597b8ad40faf
Rico Rico <rico23rico@users.noreply.github.com>
2021-11-21 23:39:14 +0100

    [FMGS] Drag predictor

--- plugins/sasl/data/modules/Custom Module/FMGS/limits.lua
+++ plugins/sasl/data/modules/Custom Module/FMGS/limits.lua
@@ -1,3 +1,20 @@
+-------------------------------------------------------------------------------
+-- A32NX Freeware Project
+-- Copyright (C) 2020
+-------------------------------------------------------------------------------
+-- LICENSE: GNU General Public License v3.0
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    Please check the LICENSE file in the root of the repository for further
+--    details or check <https://www.gnu.org/licenses/>
+-------------------------------------------------------------------------------
+-- File: FMGS/limits.lua
+-------------------------------------------------------------------------------
+
 local function compute_max_alt(curr_weight)
 
     local max_fl = Math_rescale_lim_lower(62000, 39100, 78000, 34500, curr_weight)
