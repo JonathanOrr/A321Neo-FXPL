6a2323bfa92c61bee1dc2207351418dd926e90f5
Rico Rico <rico23rico@users.noreply.github.com>
2021-11-23 22:50:48 +0100

    [FBW] Extract the green dot function for external use

--- plugins/sasl/data/modules/Custom Module/FBW/FBW_subcomponents/FBW_SYS/FAC_computation/Compute_S_F_O.lua
+++ plugins/sasl/data/modules/Custom Module/FBW/FBW_subcomponents/FBW_SYS/FAC_computation/Compute_S_F_O.lua
@@ -1,5 +1,7 @@
+include("FBW/FBW_subcomponents/FBW_SYS/FAC_computation/common_functions.lua");
+
 function update()
     set(S_speed, 1.23 * FBW.FAC_COMPUTATION.Extract_vs1g(get(Aircraft_total_weight_kgs), 0, false))
     set(F_speed, 1.22 * FBW.FAC_COMPUTATION.Extract_vs1g(get(Aircraft_total_weight_kgs), 2, false))
-    set(GD, (1.5 * get(Aircraft_total_weight_kgs) / 1000 + 110) + Math_clamp_lower((adirs_get_avg_alt() - 20000) / 1000, 0))
+    set(GD, compute_green_dot(get(Aircraft_total_weight_kgs), adirs_get_avg_alt()))
 end
