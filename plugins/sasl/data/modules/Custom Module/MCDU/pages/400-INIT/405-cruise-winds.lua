1a686029d56cf5ed8e142ab8a818d9656da34698
Rico Rico <rico23rico@users.noreply.github.com>
2022-04-29 23:51:20 +0200

    [MCDU] Basic structure of the CRUISE WIND page 405

--- plugins/sasl/data/modules/Custom Module/MCDU/pages/400-INIT/405-cruise-winds.lua
+++ plugins/sasl/data/modules/Custom Module/MCDU/pages/400-INIT/405-cruise-winds.lua
@@ -17,10 +17,71 @@
 local THIS_PAGE = MCDU_Page:new({id=405})
 
 function THIS_PAGE:render(mcdu_data)
-    self:set_title(mcdu_data, "CRZ WIND")
 
+    local legs = FMGS_get_enroute_legs()
+    assert(#legs > 0)
+
+    if not mcdu_data.page_data[405] then
+        mcdu_data.page_data[405] = {    -- Reset by 404 and 406 at every entry point
+            curr_id = 1
+        }
+    end
+
+    local leg = legs[mcdu_data.page_data[405].curr_id]
+    if not leg then -- Safety check
+        leg = legs[1] -- This always exists
+        mcdu_data.page_data[405].curr_id = 1
+    end
+
+    self:set_multi_title(mcdu_data, {
+        {txt="  CRUISE WIND " .. mcdu_format_force_to_small("AT").."         ", col=ECAM_WHITE, size=MCDU_LARGE},
+        {txt="              " .. (leg.id or "UNKN"), col=ECAM_GREEN, size=MCDU_LARGE}
+    })
+
+    self:set_updn_arrows_bottom(mcdu_data, true)
+
+    -- List of winds
+    self:set_line(mcdu_data, MCDU_LEFT, 1, "TRU WIND/ALT", MCDU_SMALL, ECAM_WHITE)
+
+
+    -- REQUEST
+    self:set_line(mcdu_data, MCDU_RIGHT, 2, "WIND", MCDU_SMALL, ECAM_ORANGE)
+    if FMGS_winds_req_in_progress() then
+        self:set_line(mcdu_data, MCDU_RIGHT, 2, "REQUEST ", MCDU_LARGE, ECAM_ORANGE)
+    else
+        self:set_line(mcdu_data, MCDU_RIGHT, 2, "REQUEST*", MCDU_LARGE, ECAM_ORANGE)
+    end
+
+    -- PAGES
+    self:set_line(mcdu_data, MCDU_RIGHT, 4, "PREV", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_RIGHT, 4, "PHASE>", MCDU_LARGE, ECAM_WHITE)
+
+    self:set_line(mcdu_data, MCDU_RIGHT, 5, "NEXT", MCDU_SMALL, ECAM_WHITE)
+    self:set_line(mcdu_data, MCDU_RIGHT, 5, "PHASE>", MCDU_LARGE, ECAM_WHITE)
     
-    self:set_line(mcdu_data, MCDU_LEFT, 4, "PAGE NOT YET IMPLEMENTED", MCDU_LARGE, ECAM_MAGENTA)
+end
+
+function THIS_PAGE:R2(mcdu_data)
+    FMGS_winds_req_go()
+end
+
+function THIS_PAGE:R4(mcdu_data)
+    mcdu_open_page(mcdu_data, 404)
+end
+
+function THIS_PAGE:R5(mcdu_data)
+    mcdu_open_page(mcdu_data, 406)
+end
+
+function THIS_PAGE:Slew_Down(mcdu_data)
+    mcdu_data.page_data[405].curr_id = (mcdu_data.page_data[405].curr_id - 1)
+    if mcdu_data.page_data[405].curr_id == 0 then
+        mcdu_data.page_data[405].curr_id = #FMGS_get_enroute_legs()
+    end
+end
+
+function THIS_PAGE:Slew_Up(mcdu_data)
+    mcdu_data.page_data[405].curr_id = (mcdu_data.page_data[405].curr_id) % (#FMGS_get_enroute_legs()) + 1
 end
 
 
