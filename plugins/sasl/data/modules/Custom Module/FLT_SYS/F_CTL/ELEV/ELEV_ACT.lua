021605fc1770a4b29b2e29fe652eeb02dae0af7d
JonathanOrr <jonathan.orr200299@gmail.com>
2022-07-20 19:17:10 +1000

    [FCTL] remove debug

--- plugins/sasl/data/modules/Custom Module/FLT_SYS/F_CTL/ELEV/ELEV_ACT.lua
+++ plugins/sasl/data/modules/Custom Module/FLT_SYS/F_CTL/ELEV/ELEV_ACT.lua
@@ -132,9 +132,6 @@ FCTL.ELEV.ACT = function (REQ_DEF, index)-- index: 1: L, 2: R
         -- No HYD at all
         local LOCAL_AIRSPD_KTS = mcos(mrad(get(Beta))) * get(TAS_ms) * 1.94384
         local DAMPING_ANGLE = mcos(mrad(get(Beta))) * ((ELEV_MAX_UP * 2) / (1 + mexp(-DAMPING_CONST * -get(Alpha))) - ELEV_MAX_UP) - get(THS_DEF)
-
-        print(mcos(mrad(get(Beta))) * -get(Alpha) - get(THS_DEF), DAMPING_ANGLE)
-
         local DAMPING_DEF_TGT = Math_rescale(0, ELEV_MAX_DN, NO_HYD_RECTR_TAS, DAMPING_ANGLE, LOCAL_AIRSPD_KTS)
         Set_dataref_linear_anim(DEF_DATAREF, DAMPING_DEF_TGT, -ELEV_MAX_UP, ELEV_MAX_DN, ELEV_NO_HYD_SPD * (1 - ELEV_STUCK))
     end
