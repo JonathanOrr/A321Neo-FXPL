158921b0baa2c89c673ea53a282415b20a9cda23
JonathanOrr <jonathan.orr200299@gmail.com>
2022-08-14 04:09:06 +1000

    [FBW] finialize grapher

--- plugins/sasl/data/modules/Custom Module/FLT_SYS/FBW/Debug/PID.lua
+++ plugins/sasl/data/modules/Custom Module/FLT_SYS/FBW/Debug/PID.lua
@@ -1,7 +1,11 @@
 local test_tbl = {
+    x = 0,
+    y = 0,
+    w = 700,
+    h = 500,
     xlim = 5, --in seconds
-    ylim = 20,
-    xbars = 5,
+    ylim = {-5, 5},
+    xbars = {1, 2, 3, 4},
     ybars = nil,
     data = {},
     dt = {},
@@ -12,8 +16,14 @@ local function Grapher_update(tbl, data_tble)
 
     --sum the elapsed time
     local time = 0
+    local dt_to_del = 0
     for i = 1, #tbl.dt do
-        time = time + tbl.dt[i]
+        if time > tbl.xlim then
+            dt_to_del = #tbl.dt - i
+            break
+        else
+            time = time + tbl.dt[i]
+        end
     end
 
     --initialize or add to value history
@@ -21,12 +31,14 @@ local function Grapher_update(tbl, data_tble)
         if not tbl.data[key] then
             tbl.data[key] = {
                 color = val.color,
+                graph = val.graph,
+                number = val.number,
                 value = {
                     val.value
                 },
             }
         else
-            if time > tbl.xlim then
+            for i = 1, dt_to_del do
                 table.remove(tbl.data[key].value, 1)
             end
 
@@ -35,61 +47,111 @@ local function Grapher_update(tbl, data_tble)
     end
 
     --process dt table
-    if time > tbl.xlim then
+    for i = 1, dt_to_del do
         table.remove(tbl.dt, 1)
     end
 
     table.insert(tbl.dt, get(DELTA_TIME))
 end
 
-local function Grapher_draw(tbl, x, y, w, h)
-    for key, val in pairs(tbl.data) do
-        local xscale = w / tbl.xlim
-        local yscale = h / tbl.ylim
 
-        local time = 0
-        for i = 1, #tbl.dt - 1 do
+local function Grapher_draw_function(tbl, funcs)
+    sasl.gl.setClipArea(tbl.x, tbl.y, tbl.w, tbl.h)
+
+    local txt_drawn = 0
+    for key, val in pairs(funcs) do
+        for i = 1, val.sample do
+            local x = Math_rescale(1, val.dom[1], val.sample, val.dom[2], i)
+            local x_next = Math_rescale(1, val.dom[1], val.sample, val.dom[2], i + 1)
+            local y = val.func(x)
+            local y_next = val.func(x_next)
+
             sasl.gl.drawLine(
-                x + time * xscale,
-                y + val.value[i] * yscale,
-                x + (time + tbl.dt[i]) * xscale,
-                y + val.value[i + 1] * yscale,
+                Math_rescale_no_lim(val.dom[1],  tbl.x,  val.dom[2], tbl.x + tbl.w, x),
+                Math_rescale_no_lim(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, y),
+                Math_rescale_no_lim(val.dom[1],  tbl.x,  val.dom[2], tbl.x + tbl.w, x_next),
+                Math_rescale_no_lim(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, y_next),
                 val.color
             )
+        end
 
-            time = time + tbl.dt[i]
+        sasl.gl.drawText(Font_B612MONO_regular, tbl.x + tbl.w - 5, tbl.y + tbl.h - 15 - 15 * txt_drawn, key, 12, false, false, TEXT_ALIGN_RIGHT, val.color)
+        txt_drawn = txt_drawn + 1
+    end
+
+    sasl.gl.resetClipArea()
+end
+
+
+local function Grapher_draw(tbl)
+    sasl.gl.setClipArea(tbl.x, tbl.y, tbl.w, tbl.h)
+    sasl.gl.drawRectangle(tbl.x, tbl.y, tbl.w, tbl.h, UI_DARK_GREY)
+
+    --darw background
+    sasl.gl.drawLine(
+        0,
+        Math_rescale(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, 0),
+        tbl.x + tbl.w,
+        Math_rescale(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, 0),
+        ECAM_GREY
+    )
+
+    if tbl.xbars then
+        for key, val in pairs(tbl.xbars) do
+            sasl.gl.drawLine(Math_rescale(0, tbl.x, tbl.xlim, tbl.x + tbl.w, val), tbl.y, Math_rescale(0, tbl.x, tbl.xlim, tbl.x + tbl.w, val), tbl.y + tbl.h, ECAM_GREY)
         end
     end
-end
 
-local function hypo_r()
-    local mcos = function (x) return math.cos(math.rad(x)) end
-    local msin = function (x) return math.sin(math.rad(x)) end
+    if tbl.ybars then
+        for key, val in pairs(tbl.ybars) do
+            sasl.gl.drawLine(tbl.x, Math_rescale(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, val), tbl.x + tbl.w, Math_rescale(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, val), ECAM_GREY)
+        end
+    end
 
-    local nz = get(Total_vertical_g_load)--mcos(get(Vpath)) / mcos(get(Flightmodel_roll))
+    --draw data lins
+    local txt_drawn = 0
+    for key, val in pairs(tbl.data) do
+        local time = 0
+        for i = 1, #tbl.dt - 1 do
+            if val.graph then
+                sasl.gl.drawLine(
+                    Math_rescale_no_lim(0,           tbl.x,    tbl.xlim, tbl.x + tbl.w,             time),
+                    Math_rescale_no_lim(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h,     val.value[i]),
+                    Math_rescale_no_lim(0,           tbl.x,    tbl.xlim, tbl.x + tbl.w, time + tbl.dt[i]),
+                    Math_rescale_no_lim(tbl.ylim[1], tbl.y, tbl.ylim[2], tbl.y + tbl.h, val.value[i + 1]),
+                    val.color
+                )
+            end
 
-    local r = (get(Weather_g) / get(TAS_ms)) * (nz * msin(get(Flightmodel_roll)) * mcos(get(Flightmodel_roll)) * mcos(get(Vpath)))
-    return (r / math.pi) * 180
-end
+            time = time + tbl.dt[i]
 
-local function alt_hypo_r()
-    local mcos = function (x) return math.cos(math.rad(x)) end
-    local msin = function (x) return math.sin(math.rad(x)) end
+            if i == #tbl.dt - 1 then
+                local txt = key
+                if val.number then
+                    txt = txt .. ": " .. Round_fill(val.value[#val.value], 4)
+                end
 
-    local nz = get(Total_vertical_g_load)--mcos(get(Vpath)) / mcos(get(Flightmodel_roll))
+                sasl.gl.drawText(Font_B612MONO_regular, tbl.x + 5, tbl.y + tbl.h - 15 - 15 * txt_drawn, txt, 12, false, false, TEXT_ALIGN_LEFT, val.color)
+                txt_drawn = txt_drawn + 1
+            end
+        end
+    end
 
-    local r = (get(Weather_g) / get(TAS_ms)) * (nz * msin(get(Flightmodel_roll)) * mcos(get(Flightmodel_roll)))
-    return (r / math.pi) * 180
+    sasl.gl.resetClipArea()
 end
 
 function update()
+    if not FBW_PID_debug_window:isVisible() then return end
+
     Grapher_update(test_tbl, {
-        Q  = {color = ECAM_BLUE,  value = get(Flightmodel_r_deg)},
-        hypo_r = {color = ECAM_GREEN, value = hypo_r()},
-        alt_hypo_r = {color = ECAM_ORANGE, value = alt_hypo_r()},
+        G = {graph = true, number = true, color = ECAM_BLUE, value = get(Total_vertical_g_load)},
+        Q = {graph = true, number = true, color = ECAM_RED, value = get(Flightmodel_q_deg)},
     })
 end
 
 function draw()
-    Grapher_draw(test_tbl, 0, 250, 700, 500)
+    Grapher_draw(test_tbl)
+    Grapher_draw_function(test_tbl, {
+        sin = {color = ECAM_MAGENTA, func = function(x) return math.sin(math.rad(x)) end, dom = {-180, 180}, sample = 50},
+    })
 end
